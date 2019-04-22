657ece24a8df998cd9fbd25a81f1aefac3339c7d
JON TOSHMATOV <jontoshmatov@yahoo.com>
2019-04-22 18:28:00 -0400

    Class 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>Mario.inc</title>
+</head>
+<body>
+<div>
+    <h3>Amazon - Umida Search</h3>
+    <form action="backend.php" method="get">
+        <p>
+            <input name="keyword" type="text" value="" placeholder="your keyword">
+
+            <input type="submit" value="Search">
+
+        </p>
+    </form>
+</div>
+
+
+<!--
+
+I am a HTML comment
+
+-->
+
+</body>
+</html>
