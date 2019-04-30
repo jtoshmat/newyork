346f12c0a5370d6c7bbbdd5b8b53a7ca9568bf27
Jon Toshmatov - System Admin <jontoshmatov@yahoo.com>
2019-04-29 20:59:48 -0400

    cases exercise is done

---
+++ cases.php
@@ -0,0 +1,38 @@
+<?php
+    $states = $_GET['state']??'Iowa';
+    $states2 = $_GET['state2']??'Texas';
+
+
+
+    echo "<hr>";
+    echo "<h3>Switch Case <br>";
+
+    switch ($states){
+        case 'Iowa':
+            echo "Hello Iowa!";
+            break;
+
+        case 'California':
+            echo "Welcome to Golden State!";
+            break;
+
+        default:
+            echo "Welcome to whatever state!";
+            break;
+    }
+
+
+    echo "<hr>";
+    echo "<h3>If Statement";
+    echo "<br>";
+
+    if ($states=='Iowa'){
+        echo "Hello Iowa! If Statement";
+    }
+    if ($states=='California'){
+        echo "Welcome to Golden State!";
+    }
+
+    if ($states=='Florida' or $states2=='Texas'){
+        echo "Welcome to Sunshine State!";
+    }
