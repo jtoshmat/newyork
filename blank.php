ae5d5742e6b3389c26a4a82354822223f0129fb4
Jon Toshmatov - System Admin <jontoshmatov@yahoo.com>
2019-04-30 16:18:52 -0400

    created a blank page

---
+++ blank.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Calculator</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <script src="/js/bootstrap.js"></script>
+</head>
+<body>
+
+<h1>Blank Page</h1>
+
+</body>
+</html>
