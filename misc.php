9bafa6a0551db89a73afd35724de7663031aa144
Jon Toshmatov - System Admin <jontoshmatov@yahoo.com>
2019-05-01 17:50:45 -0400

    Showed how bootstrap works to Parvina

---
+++ misc.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Calculator</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <script src="/js/bootstrap.js"></script>
+</head>
+<body>
+
+<h1>Blank Page</h1>
+
+<p>
+    Team, <br>
+
+    Please use this blank page as your new starting page.
+
+
+</p>
+
+
+<form>
+    <p>
+        <select>
+            <option>Tashkent</option>
+            <option>Seattle</option>
+            <option>New York</option>
+            <option>Paris</option>
+        </select>
+    </p>
+    <p>
+        Male: <input type="checkbox" value="Male">
+
+        Female: <input type="checkbox" value="Female">
+    </p>
+
+    Democrat: <input type="radio" name="political" value="d">
+    Republican: <input type="radio" name="political" value="r">
+
+    <p><input></p>
+    <p>
+        <input class="btn btn-info" type="reset">
+        <input class="btn btn-primary" type="submit">
+    </p>
+</form>
+
+
+</body>
+</html>
