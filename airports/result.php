6f16c27904d969898f7787cb05558cb19f1cd1a1
Stan <Stan@Stan.local>
2019-05-14 23:14:57 -0400

    I just created my airports directory 222

---
+++ airports/result.php
@@ -0,0 +1,9 @@
+<?php
+
+$parms = $_POST;
+
+
+foreach ($parms as $field=>$value){
+    echo $field." = ".$value;
+    echo "<hr>";
+}
--- airports/search.php
+++ airports/search.php
@@ -1,1 +1,27 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <title>AIRPORT SEARCH PAGE</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <div>
+        <h1>
+            SEARCH AIRPORTS AROUND THE GLOBE
+        </h1>
+    </div>
+
+</head>
+<body>
+<div id="frm">
+    <form action="/airports/result.php" method="POST">
+        <p>
+            <label> ENTER COUNTRY NAME </label>
+            <input type="text" id="country" name="country" />
+        </p>
+        <p>
+            <input type="submit" id="btn" value="SEARCH" />
+        </p>
+    </form>
+</div>
+
+</body>
+</html>
