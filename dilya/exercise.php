8427c527b8f167753d8dd9dbfeecf1578fcd57c3
omran83 <aldweikato83@gmail.com>
2019-05-22 16:32:15 -0500

    saving my work

---
+++ dilya/exercise.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>WELCOME!</h1>
+
+<img src="https://gocity.com/chicago/sites/g/files/ufrfmb176/files/styles/tile_pass_product_mobile/public/Chicago-All-Inclusive-Pass.jpg">
+
+<hr>
+<button>CLICK ME</button>
+<br>
+<a href="http://google.com">Go to Google</a>
+
+
+<style>
+
+</style>
+
+</body>
+</html>
---
+++ dilya/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dilya's Webpage!</title>
+    <link href="../css/dilya.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="header">
+Blank Company Template
+</div>
+
+<div id="nav">
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Products</li>
+        <li>Contact Us</li>
+    </ul>
+</div>
+
+<div id="content">
+
+    <p>This is 1st parahraph</p>
+    <p>This is 2nd paragraph</p>
+
+</div>
+
+<div id="footer">
+
+</div>
+
+</body>
+</html>
+
