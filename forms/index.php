8ac1cffa78a7938727a4755213cbf33a077f84f9
Jon Toshmatov - System Admin <jontoshmatov@yahoo.com>
2019-06-22 20:05:30 -0400

    saving my js form

--- forms/index.php
+++ forms/index.php
@@ -1,43 +1,20 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <link href="/css">
-=======
->>>>>>> 90ca4a43df758b599ba84f9546d7041677077d8e
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <title>Document</title>
 </head>
 <body>
 
-=======
-    <title>My CSS FORM EXERCISE</title>
-    <link href="/css/bootstrap.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div class="container">
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
->>>>>>> 90ca4a43df758b599ba84f9546d7041677077d8e
+<form method="post" action="submit.php">
+    <p><input name="first_name" value="" type="text" placeholder="First Name"></p>
+    <p><input name="last_name" value="" type="text" placeholder="Last Name"></p>
+    <p><input name="age" value="" type="number" placeholder="Age"></p>
+    <p><input type="submit" value="Submit"></p>
+</form>
+
 </body>
 </html>
