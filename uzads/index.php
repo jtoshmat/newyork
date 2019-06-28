0d4491df9336f8fdf3d3b2e809f06bf72d53097f
Jon Toshmatov <jontoshmatov@Jons-Air.home>
2019-06-27 21:27:54 -0400

    Thursday save

---
+++ uzads/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UzAds Project</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/uzads/css/style.css" rel="stylesheet">
+    <script src="/js/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="/js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div id="header">
+    <div id="leftcol">
+        <img title="Welcome to UzAds" alt="My Logo" src="/uzads/img/uztel.png">
+    </div>
+    <div id="middlecol">
+        <form>
+            <input class="form-control" name="keyword" type="text" placeholder="Enter your keyword">
+        </form>
+    </div>
+    <div id="rightcol">
+        <img title="Click for Settings" alt="Gear" src="/uzads/img/gear_icon.png">
+        <img title="Click for Help" alt="Gear" src="/uzads/img/help_icon.png">
+        <img title="Click for Alerts" alt="Gear" src="/uzads/img/pc_icon.png">
+        <img title="Click for Alerts" alt="Gear" src="/uzads/img/pc_icon.png">
+        <img title="Click for Alerts" alt="Gear" src="/uzads/img/pc_icon.png">
+    </div>
+    <div class="myclear"></div>
+    <div id="bottomcol">
+        <?php include_once 'navbar.php'; ?>
+    </div>
+</div>
+<!-- ############ End of Header ################### -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="leftpart">Left Part</div>
+<div id="rightpart">Right Part</div>
+<div id="footer">Footer</div>
+</body>
+</html>
