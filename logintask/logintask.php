84dad2c039f787de103017f8b16dfe540ed9ee7e
Muzaffar Khusnitdinov <muzaffar@Muzaffars-MBP.home>
2019-07-18 17:21:13 -0400

    finish project

---
+++ logintask/logintask.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/logintask/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Logintask</title>
+</head>
+<body>
+
+<div class="log_form">
+    <h1>Login</h1>
+    <form action="">
+        <input class="name" type="text" title="username" placeholder="username"/><br>
+        <input class="psw" type="password" title="username" placeholder="password"/><br>
+        <input class="checkbox" type="checkbox">Remember me
+        <a class="forgot" href="#">Forgot Password?</a><br>
+        <button class="btn" type="login">Login</button>
+
+    </form>
+    <div class="box">
+    <h2>Login</h2>
+    <p><strong>With your social media account</strong></p>
+
+        <div class="wrapper">
+            <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-google-plus"></i></a>
+            <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-facebook-square"></i></a>
+            <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-twitter-square"></i>
+        </div></a>
+        <p class="myp"><strong>Don't have an Account? Register Now!</strong></p>
+        <button class="register" type="Register">Register</button>
+
+    </div>
+</div>
+
+</body>
+</html>
--- uzads/index.php
+++ uzads/index.php
@@ -37,50 +37,126 @@
 <!-- ############ End of Header ################### -->
 
 
-
 <div id="leftpart">
 
-<div class="quickfind">
-    <fom>
-        <input class="input-group" name="keyw" type="text">
-    </fom>
+    <div class="quickfind">
+        <fom>
+            <input class="input-group" name="keyw" type="text">
+        </fom>
+    </div>
+
+    <div class="leftmenu">
+
+        <span>Service Setup Home</span>
+
+        <ul class="father">
+            <li data-row="0">ADMINISTRATION
+                <ul class="child">
+                    <li>Users</li>
+                    <li>Channels</li>
+                    <li>Knowledge</li>
+                    <li>Data Categories</li>
+                </ul>
+            </li>
+            <li data-row="1">PLATFORMS TOOLS
+                <ul class="child">
+                    <li>Custome Code</li>
+                    <li>Environments</li>
+                </ul>
+
+            </li>
+            <li data-row="2">AUTOMATION
+                <ul class="child">
+                    <li>Process Automation</li>
+                </ul>
+            </li>
+            <li data-row="3">USER INTERFACE
+                <ul class="child">
+                    <li>Objects and fields</li>
+                    <li>User Interface</li>
+                    <li>Reports and Dashboards</li>
+                </ul>
+            </li>
+            <li data-row="4">MOBILE
+                <ul class="child">
+                    <li>Mobile Apps</li>
+                </ul>
+            </li>
+            <li data-row="5">SETTINGS
+                <ul class="child">
+                    <li>Entitlement Management</li>
+                    <li>Field Service</li>
+                    <li>Service Cloud Einstein</li>
+                    <li>Data</li>
+                    <li>Company Settings</li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
+</div>
+<!-- ############ End of Header ################### -->
+
+
+<div id="rightpart">
+    <p>Recommended Setup</p>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <p>Add Your Users</p>
+                <p>Add Your team to your Service Cloud in this simple setup flow</p>
+                <button type="button" name="Get Started">Get Started</button>
+            </div>
+            <div class="col-sm">
+                <p>Customised Case Status</p>
+                <p>Track and streamline your agent caseloads</p>
+                <button type="button" name="Get Started">Get Started</button>
+            </div>
+
+            <div class="col-sm jontosh">
+                <p><strong>Email Setup</strong><br>Turn your emails into your cases</p>
+                <p><strong>Knowledge Setup</strong><br>Set up your Knowledge Base in Service Cloud</p>
+                <p><strong>Lighting Communities Setup</strong><br>Build a community where customers help themselves</p>
+            </div>
+
+        </div>
+    </div>
 </div>
 
-<div class="leftmenu">
 
-    <span>Service Setup Home</span>
+<!-- ############ End of Header ################### -->
 
-    <ul class="father">
-        <li data-row="0">Administration
-            <ul class="child">
-                <li>Users</li>
-                <li>Channels</li>
-                <li>Knowledge</li>
-                <li>Data Categories</li>
-            </ul>
-        </li>
-        <li data-row="1">Platform Tools
-            <ul class="child">
-                <li>Custome Code</li>
-                <li>Environments</li>
-            </ul>
+<div id="footer">
+    <div class="container2">
+        <div class="row2">
+            <div class="col-sm2">
+                <img class="image" src="/img/mylogo.png">
+                <p class="myp">No cases to show<br>Tackle some cases and watch your performance data grow into useful
+                    insights for your team</p>
+            </div>
+        </div>
+    </div>
 
-        </li>
-        <li data-row="2">Automation</li>
-        <li data-row="3">User Interface</li>
-    </ul>
-
+    <div class="row3">
+        <span class="row3_p"><strong>Current Usage</strong></span>
+        <table class="row3_table">
+            <tr>
+                <td>Salesforce Licenses<br>1 / 1</td>
+                <td>Service Licenses<br>1 / 1</td>
+            </tr>
+        </table>
+        <ul class="row3_column">
+            <li data-row="0">Service features</li>
+            <li data-row="1">Storage</li>
+            <li data-row="2">Other</li>
+        </ul>
+    </div>
 </div>
 
-
-
+</div>
+</div>
 </div>
 
-
-
-
-
-<div id="rightpart">Right Part</div>
-<div id="footer">Footer</div>
 </body>
 </html>
