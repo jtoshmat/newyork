5a45de3e2bee82f3f583c300b9bfbc9a06beaba6
ABDULLO <oabdullo@gmail.com>
2019-07-18 20:01:22 -0400

    im saving my work

---
+++ gulya78/login page.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<meta name=" viewport
+content= "width ">
+<head>
+    Modal Login Form
+</head>
+
+<meta name=" viewport"
+content="width=device-width, initial-scale=1">
+<style>
+    body{ font-family: Arial, Helvetica, sans-serif;}
+/* Full-width input fields */
+    input[type=text], input[type=password]{
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block ;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+
+/* Set a style for all buttons */
+    button { background-color: #4caf50;
+    color: white;
+    padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+    }
+</style>
+
+
+
+
+
+
+</html>
