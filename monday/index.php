c8cfeaed12600b83f92cb89d20aadd414b45802c
Rustam Mirakbarov <rustammirakbarov@Rustams-MBP.home>
2019-07-22 19:02:36 -0400

    Rustam is saving Monday project

---
+++ monday/index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>fish</title>
+    <link href="/css/bootstrap.css" rel="stylesheet"
+</head>
+<body>
+<div class="container-fluid">
+    <div class="topheader">
+        Top header
+    </div>
+    <div class="mycarousel">
+        Div Image
+    </div>
+    <div class="mycols">
+        <div class="col1">
+            <table>
+                <tr>
+                    <td>Host1</td>
+                </tr>
+                <tr>
+                    <td>Free</td>
+                </tr>
+                <tr>
+                    <td>sfsdfs</td>
+                </tr>
+                <tr>
+                    <td>Dominio</td>
+                </tr>
+                <tr>
+                    <td><button class="btn btn-primary">Buy Now</button></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col2">
+            <table>
+                <tr>
+                    <td>Host 2</td>
+                </tr>
+                <tr>
+                    <td>Free</td>
+                </tr>
+                <tr>
+                    <td>sfsdfs</td>
+                </tr>
+                <tr>
+                    <td>Dominio</td>
+                </tr>
+                <tr>
+                    <td><button class="btn btn-primary">Buy Now</button></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col3">
+            <table>
+                <tr>
+                    <td>Host3</td>
+                </tr>
+                <tr>
+                    <td>Free</td>
+                </tr>
+                <tr>
+                    <td>sfsdfs</td>
+                </tr>
+                <tr>
+                    <td>Dominio</td>
+                </tr>
+                <tr>
+                    <td><button class="btn btn-primary">Buy Now</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="footer">footer</div>
+
+
+</div>
+
+<style>
+    .footer {
+    }
+
+    .col1 {
+        width: 33%;
+        float: left;
+
+    }
+
+    .col2 {
+        width: 34%;
+        float: left;
+    }
+
+    .col3 {
+        width: 33%;
+        float: left;
+
+    }
+    .col1 table{width: 100%
+    }
+
+
+    body {
+        background-color: #000000;
+        color: #fff3cd;
+    }
+
+    .topheader {
+        width: 100%;
+        height: 50px;
+        background-color: #5a616a;
+
+    }
+
+    .mycarousel {
+        width: 100%;
+        height: 300px;
+        background-color: #005cbf;
+        background-image: url("monday.png");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    }
+
+    table{
+        width: 100%;
+    }
+
+    .mycols {
+        width: 100%;
+        height: 200px;
+
+        background-color: white;
+        color: black;
+    }
+    table tr:nth-child(1){
+        background-color: #0b2e13;
+color: #fff3cd;
+        text-align: center;
+    }
+    table tr:nth-child(2){
+        background-color: #d8d8d8;
+    }
+    table tr:nth-child(5){
+        background-color: #d8d8d8;
+        text-align: center;
+    }
+
+
+
+</style>
+
+
+</body>
+</html>
