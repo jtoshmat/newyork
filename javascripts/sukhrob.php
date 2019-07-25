5b34dcf11a987e718cdb2ef53e5d39b9bd6f06d7
Jon Toshmatov - System Admin <jontoshmatov@yahoo.com>
2019-07-25 10:54:59 -0400

    sukhrob js class

---
+++ javascripts/sukhrob.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Javascript Exercises</title>
+</head>
+<body>
+<h1>My Javascripts</h1>
+
+
+<div id="demo">
+
+</div>
+
+
+
+<style>
+    #demo{
+        background-color: #fcffd2;
+        width:100%;
+        height: 100px;
+        border:1px solid #000;
+    }
+
+    .myhtml{
+        color:#f00;
+        border:1px dotted #00e200;
+        background-color: #005cbf;
+    }
+
+
+</style>
+<script src="sukhrob.js" type="text/javascript"></script>
+</body>
+</html>
--- monday/index.php
+++ monday/index.php
@@ -90,6 +90,7 @@
         margin:0;
         padding:0
         width: 100%;
+        height:100%;
         font-family: "American Typewriter";
         font-size: 14px;
     }
