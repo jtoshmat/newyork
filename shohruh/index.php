008b2eb647043405ad2440d75dfc7ee07885fd7d
MY-IT-EDUCATION <my-it-education@MY-IT-EDUCATIONs-MacBook-Pro.local>
2019-08-16 19:42:18 -0400

    saving shohruhs code

--- shohruh/index.php
+++ shohruh/index.php
@@ -6,8 +6,104 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shohruh's Site</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-<h1>Welcome to Shohruh!</h1>
+
+
+<div class="container">
+    <h4 style="text-align: center">Figure 20.</h4>
+
+    <table>
+
+        <tr>
+            <td></td>
+            <td >Across the enterprise</td>
+            <td>Within <br> individual <br> business</td>
+            <td>Work in progress</td>
+            <td>No</td>
+        </tr>
+
+        <tr>
+            <td>Less than $50m</td>
+            <td>30%</td>
+            <td>26%</td>
+            <td>33%</td>
+            <td>10%</td>
+        </tr>
+
+        <tr>
+            <td>$50m - $2050</td>
+            <td>30%</td>
+            <td>32%</td>
+            <td>32%</td>
+            <td>6%</td>
+        </tr>
+
+        <tr>
+            <td>More than $250</td>
+            <td>46%</td>
+            <td>27%</td>
+            <td>21%</td>
+            <td>6%</td>
+        </tr>
+
+
+        <tr>
+            <td colspan="5" class="lasttd">Figure 20. Does your organization have a clear digitial business vision and strategry</td>
+        </tr>
+
+    </table>
+
+
+</div>
+
+
+<style>
+
+    body{
+        font-family: Verdana;
+        font-size: 14px;
+    }
+
+    td:first-child{
+        background-color: #d1d1d1;
+    }
+
+    td:nth-child(2){
+        background-color: #8dd172;
+    }
+    td:nth-child(3){
+        background-color: #99e97d;
+    }
+    td:nth-child(4){
+        background-color: #eff1b5;
+    }
+    td:nth-child(5){
+        background-color: #f1cccb;
+    }
+
+    table{
+        width:100%;
+    }
+
+    td{
+        margin:5px;
+        padding: 5px;
+        border: 1px solid #000;
+        text-align: center;
+    }
+
+    .lasttd{
+        text-align: left;
+        border:none;
+        font-size: 16px;
+        color: #5c5c5c;
+        font-family: "American Typewriter";
+        background-color: #fff !important;
+    }
+
+</style>
+
 </body>
 </html>
