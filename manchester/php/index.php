0291691fb12d1002d1152e20d6f0f99ea50bc03f
MY-IT-EDUCATION.US <my-it-education@MYITEDUONUSsMBP.home>
2019-09-01 16:36:36 -0400

    creating a new project for Uber team

--- manchester/php/index.php
+++ manchester/php/index.php
@@ -1,25 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to PHP</title>
-</head>
-<body>
-<h4>Welcome to PHP Class!</h4>
-
 <?php
 
-echo "Hello World!";
-echo "<hr>";
-print "Hello World!";
+$result = $number??10;
 
-?>
+echo $result;
 
 
-
-
-</body>
-</html>
