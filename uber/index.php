2420a20b4f0722c1f0a95c4df69460228226d092
MY-IT-EDUCATION.US <my-it-education@MYITEDUONUSsMBP.home>
2019-09-01 17:00:49 -0400

    saving initial file

---
+++ uber/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Uber</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <img src="/img/office1.jpg">
+    <img src="/img/office2.jpg">
+    <img src="/img/office3.jpg">
+    <img src="/img/office4.jpg">
+</div>
+
+<style>
+
+    body{
+        background-color: #030300;
+    }
+
+
+    img {
+        width:300px;
+        height:200px;
+        border:2px solid #915221;
+        border-radius: 15px;
+        box-shadow: #ed673f 5px 5px 5px 5px;
+        opacity: 0.5;
+    }
+
+    img:hover{
+        opacity: 1;
+        cursor: pointer;
+        box-shadow: #5576ed 5px 5px 5px 5px;
+    }
+
+</style>
+
+</body>
+</html>
