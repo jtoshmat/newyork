76fdcfff1c48d597f02ccc0974f0391c0d5cd7b4
Farangiz Sadykova <farangizsadykova@Farangizs-MBP.home>
2019-09-14 16:11:03 -0400

    saving

---
+++ uber/Repeat.php
@@ -0,0 +1,166 @@
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homework</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    
+    
+
+<div class="links">
+
+   <h1>Welcome to our web page.</h1>
+    
+  <a href="http://google.com">Google</a>
+    <a href="http://adidas.com">Adidas</a>
+    <a href="http://Nike.com">Nike</a>
+    <a href="http://amazon.com">Amazon</a>
+    
+    <br>
+
+    <img src="adidas.php">
+    <img src="nike.php">
+    <img src="google.php">
+    <img src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2Fce09e870cef94623a513fd6e0eb9258c%2F960x0.jpg%3Ffit%3Dscale">
+
+</div>
+
+    <div class="mytable">
+        <table class="table">
+<tr>
+    <td>Product</td>
+    <td>2</td>
+    <td>3</td>
+    <td>4</td>
+    <td>5</td>
+    <td>6</td>
+    <td>7</td>
+    <td>8</td>
+    <td>9</td>
+
+</tr>
+
+        <tr>
+            <td>Size</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+        </tr>
+
+            <tr>
+                <td>Price</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+            </tr>
+
+            <tr>
+                <td>Policy</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+            </tr>
+
+        </table>
+
+    </div>
+
+</div>
+    
+<style>
+    h1{
+        color: blue;
+        text-align: center;
+        font-style: initial;
+    }
+
+    body{
+        background-color: #ffe283;
+    }
+    .links{
+       text-align: center ;
+        margin: 40px;
+    }
+
+    a{
+        margin: 5px;
+        padding 5px;
+        border: 1px solid #ff3333;
+        border-radius: 5px;
+        background-color: #253440;
+        color: white;
+    }
+
+    a:hover {
+        color: black;
+        background-color: red;
+    }
+
+    img{
+        width: 300;
+        height: 200;
+        border-radius: 15px;
+        border-color: #1c2e41;
+        margin: 15px;
+        box-shadow: #7abaff 7px 7px 7px;
+    }
+
+    img:hover{
+        opacity: 0.1;
+        cursor:pointer;
+        box-shadow: #4fff33 5px 5px 5px 5px ;
+    }
+
+    tr:nth-child(1){
+        background-color: #7abaff;
+    }
+
+    tr:nth-child(2) {
+      background-color: #81f0ff;
+    }
+
+    tr:nth-child(3){
+        background-color: #8e473e;
+    }
+
+    tr:nth-child(4){
+        background-color: #b02d27;
+    }
+
+    table{
+        font-size: small;
+        font-style: italic;
+        background-color: tomato;
+        border: solid 1px #353741;
+        box-shadow: red 5px 5px 5px 5px;
+        border-collapse: separate;
+
+
+    }
+
+</style>
+
+</body>
+</html>
