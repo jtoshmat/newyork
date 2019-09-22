0d3a568aba192a242a7bbcd2c682e104b2b1dbe9
sher <friend802000@gmail.com>
2019-09-22 12:44:32 -0400

    saving my work

---
+++ php/emails.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <title>emails Report</title>
+</head>
+<body>
+<div class="container">
+    <div class="mytable">
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Domain</td>
+                <td>Email</td>
+            </tr>
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Domain</td>
+                <td>Email</td>
+            </tr>
+        </table>
+
+
+    </div>
+</div>
+
+<style>
+    table {
+        background-color: #003f80;
+    }
+
+
+</style>
+
+
+</body>
+</html>
