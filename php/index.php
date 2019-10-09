ae6fb802820e9e03f7637d05b89e0b9dd0d96cea
MY-IT-EDUCATION.US <my-it-education.us@MYITEDUONUSsAir.home>
2019-10-08 20:04:09 -0400

    saving

--- php/index.php
+++ php/index.php
@@ -1,16 +1,88 @@
 <?php
-$numbers = range(1,100);
+namespace Family;
 
-foreach ($numbers as $number){
-    $odd = $number % 2;
+class Father{
 
-    if (!$odd) {
-        echo " * ";
-        echo $number . " : " . $odd . "<hr>";
+    public $parms;
+
+    public function __construct()
+    {
+        $this->parms = $_GET;
     }
+
+
+    public function getAirports(){
+        $this->parms['name'] = $this->parms['name']??null;
+        switch($this->parms['name']){
+            case 'Jon':
+                echo "Howdy Jon, How are things?";
+                break;
+
+            case 'Sukhrob':
+                echo "Salom Sukhrobjon, hammasi yaxshimi?";
+                break;
+
+            default:
+                echo "No name was passed";
+                break;
+        }
+
+
+
+    }
+
 
 }
 
+$father = new Father();
+?>
 
-//PHP Basics Variables: (Strings, int, NULL, Floats, Boolean, Arrays, Resources), Operations
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
 
+<div class="container-fluid">
+
+    <div class="cols">
+        <a href="index.php?name=Jon">Jon Toshmatov</a>
+    </div>
+    <div class="cols">
+        <a href="index.php?name=Sukhrob">Sukhrob Shadiyarov</a>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <p>
+        <hr>
+        <?=$father->getAirports()?>
+    <hr>
+    </p>
+
+</div>
+
+
+<style>
+    .cols{
+        width:300px;
+        border:1px solid black;
+        background-color: #46060c;
+        margin:auto;
+        float: left;
+        text-align: center;
+    }
+
+    a{
+        color: #ffcf81;
+    }
+</style>
+
+</body>
+</html>
--- umida/index.php
+++ umida/index.php
@@ -1,74 +1,36 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML and CSS</title>
-</head>
-<body>
-
-<div id="main">
-
-<div class="img1 myimg">
-    <span class="myspan">HELLO WORLD</span>
-    <img class="myimage" src="https://upload.wikimedia.org/wikipedia/commons/b/be/Port_of_Dubuque.jpg"></div>
-<div class="img2 myimg">
-    <span class="myspan">HELLO WORLD 2222</span>
-    <img class="myimage" src="http://downtowndubuque.org/wp-content/uploads/2015/04/Dubuque-Farmers-Market1-Dubuque-IA.jpg"></div>
-<div class="img3 myimg">
-    <span class="myspan">HELLO WORLD 33333</span>
-    <img class="myimage" src="https://upload.wikimedia.org/wikipedia/commons/8/84/Dubuque_IA_-_overview.jpg"></div>
-<div class="img4 myimg">
-    <span class="myspan">HELLO WORLD  4444</span>
-    <img class="myimage" src="http://thebigphotos.com/wp-content/uploads/2018/07/dubuque-iowa.jpg"></div>
-</div>
+<?php
+namespace Gaybullaevs;
+include_once 'Myfather.php';
+include_once 'MyMother.php';
+include_once 'startbusiness.php';
+include_once 'Traits/ToolsTrait.php';
 
 
-<style>
-.myspan{
-    position: relative;
-    left: 20%;
-    top:10%;
-    background-color: #d7d7d7;
-    padding:5px;
-    margin: 5px;
-    z-index: 100;
+class Son extends Startbusiness implements \Myfather, \MyMother {
+
+    use \ToolsTrait;
+
+    public function ogorodniChop()
+    {
+        return "Hop ada, ogorodni chopib quydim!";
+    }
+
+    public function sellBull()
+    {
+        return "Novosni $2500 ga sotdim";
+    }
+
+    public function marryUzbekGirl()
+    {
+        return "Hop oyjon, Uzbek qiziga uylanaman!";
+    }
+
+    public function noMoreHangOutAtNight()
+    {
+        return "Adajon, uzr, mani kechirin, boshqa qaytailmidi!";
+    }
+
 }
 
-.myimage:hover > .myspan{
-    display: none;
-    z-index: 10000;
-}
-
-
-
-    #main{
-        background-color: #005cbf;
-        width: 80%;
-        margin: auto;
-    }
-    .myimage{
-        width: 30%;
-        height: 100%;
-        border: 3px dotted #f52a3a;
-        padding: 20px;
-        background-color: #ffa791;
-        margin: 3px;
-        z-index: 99;
-    }
-
-
-    body{
-        background-color: #abdde5;
-    }
-</style>
-
-
-
-
-
-
-</body>
-</html>
+$son = new Son();
+echo $son->buildBusiness();
