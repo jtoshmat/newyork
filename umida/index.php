cb86266a31685d07f6715268fd2dfe6cdbbaf1bb
MY-IT-EDUCATION.US <my-it-education.us@MYITEDUONUSsAir.home>
2019-10-10 13:23:32 -0400

    Login form is created

--- umida/index.php
+++ umida/index.php
@@ -1,36 +1,3 @@
 <?php
-namespace Gaybullaevs;
-include_once 'Myfather.php';
-include_once 'MyMother.php';
-include_once 'startbusiness.php';
-include_once 'Traits/ToolsTrait.php';
 
-
-class Son extends Startbusiness implements \Myfather, \MyMother {
-
-    use \ToolsTrait;
-
-    public function ogorodniChop()
-    {
-        return "Hop ada, ogorodni chopib quydim!";
-    }
-
-    public function sellBull()
-    {
-        return "Novosni $2500 ga sotdim";
-    }
-
-    public function marryUzbekGirl()
-    {
-        return "Hop oyjon, Uzbek qiziga uylanaman!";
-    }
-
-    public function noMoreHangOutAtNight()
-    {
-        return "Adajon, uzr, mani kechirin, boshqa qaytailmidi!";
-    }
-
-}
-
-$son = new Son();
-echo $son->buildBusiness();
+var_dump($_REQUEST);
