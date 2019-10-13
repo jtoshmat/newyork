9642956fe83487c0fb63b343f81a1b9d07499ca7
Farkhod khikmatov <khikmatov.farkhod@gmail.com>
2019-10-13 17:46:32 -0400

    fgdfgf

--- demo/index.php
+++ demo/index.php
@@ -1,77 +1,14 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Demo Project</title>
-	<link href="/css/bootstrap.css" rel="stylesheet">
-</head>
 <body>
 
-	<?php
-
-
-include "../inc/database.php";
-
-$db  = new database();
-$airports = $db->sql("select * from aiports");
-var_dump($airports);
-
-
-
-	?>
-
-<div class="container">
-
-<div class="airports">
-
-<h3> World Airports </h3>
-
-<table class="table table-bordered">
-<tr>
-	<td>ID</td>
-	<td>City</td>
-	<td>Country</td>
-	<td>Code</td>
-</tr>
-
-<tr>
-	<td>ID</td>
-	<td>City</td>
-	<td>Country</td>
-	<td>Code</td>
-</tr>
-
-
-</table>
-
-
-</div>
-
-
-
-<style>
-
-	tr:first-child{
-		background-color: blue;
-		color:white;
-	}
-
-	table{
-		background-color: white;
-
-	}
-
-.airports{
-	width:80%;
-	background-color: lightblue;
-	border:1px solid black;
-	margin:auto;
-	padding:5px;
-}
-
-body{
-}	
-</style>
-
-
+<form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+<?php
+echo $_POST["name"];
+echo $_POST["email"];
+?>
 </body>
 </html>
