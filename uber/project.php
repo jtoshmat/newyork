4b91ff74f6639d8a75b307bd9225de8fe6bb6ca6
Ansar Rakhmankulov <ansar4clip@gmail.com>
2019-10-17 19:41:46 -0400

    saving before merge

---
+++ uber/project.php
@@ -0,0 +1,20 @@
+<?php
+namespace Oila;
+abstract class Project{
+    public function rentedStore(){
+        return "Dad rented a store by Toshtemir's house.";
+    }
+
+    public function recivedCredit(){
+        return "Applied";
+    }
+
+    public function purchasedEquipment(){
+        return "Dad purchased all the equipments";
+    }
+
+}
+
+
+
+
