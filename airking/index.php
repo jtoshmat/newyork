023473183f18d35e29483f127d77579cbb9946c9
Asror Umarov <asrorumarov@Asrors-MBP.home>
2019-10-23 17:43:53 -0400

    saving your code or your own message

---
+++ airking/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Api Test</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+
+<div class="container-fluid">
+    <h3>AirKing Travel Package</h3>
+    <form action="search.php">
+        <p>From: <input name="from_city" type="text"></p>
+        <p>To: <input name="to_city" type="text"></p>
+        <  p><button>Search</button></p>
+    </form>
+</div>
+
+
+</body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<body>
+
+<h1>welcome to Asror firts site</h1>
+<h2>welcome to Asror firts site</h2>
+<h3>welcome to Asror firts site</h3>
+<h4>welcome to Asror firts site</h4>
+<h5>welcome to Asror firts site</h5>
+<h6>welcome to Asror firts site</h6>
+
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy
+    text ever since the 1500s, when an unknown printer took a
+    galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into
+    electronic typesetting, remaining essentially unchanged.
+    It was popularised in the 1960s with the release of Letraset
+    sheets containing Lorem Ipsum passages, and more recently with
+    desktop publishing software like Aldus PageMaker including
+    versions of Lorem Ipsum.</p>
+
+<a href="http://www.myiteducation.org">click here</a>
+<h3>my website</h3>
+<table class="table table-bordered">
+ <tr>
+     <td>january</td>
+     <td>febuary</td>
+     <td>march</td>
+     <td>april</td>
+ </tr>
+    <tr>
+        <td>may</td>
+        <td>june</td>
+        <td>july</td>
+        <td>august</td>
+    </tr>
+    <tr>
+        <td>september</td>
+        <td>oktober</td>
+        <td>november</td>
+        <td>december</td>
+    </tr>
+    <tr>
+        <td><img  class="mypic" src="/html/img/rolex.jpg"> </td>
+        <td>oktober</td>
+        <td>november</td>
+        <td>december</td>
+    </tr>
+
+</table>
+<table class="table table-bordered colaction">
+    <tr>
+        <th>No</th>
+        <th>Models</th>
+        <th>Prices</th>
+        <th>Pictures</th>
+    </tr>
+    <tr>
+ <td>1</td>
+ <td>tesla modelX</td>
+ <td>200,000</td>
+ <td><img class="mypic" src="/html/img/teslaMX.jpg"> </td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>bmw m8</td>
+    <td>500,000</td>
+    <td><img class="mypic" src="/html/img/bmwM8.jpeg"> </td>
+</tr>
+<tr>
+    <td>3</td>
+    <td>hublet watch</td>
+    <td>65,000</td>
+    <td><img class="mypic" src="/html/img/hublet1"> </td>
+</tr>
+<tr>
+    <td>4</td>
+    <td>honda accord</td>
+    <td>35,000</td>
+    <td><img class="mypic" src="/html/img/hondaAC"> </td>
+</tr>
+</table>
+
+<table class="table table-bordered first project">
+    <tr>
+        <th>no</th>
+        <th>models</th>
+        <th>prices</th>
+        <th>pictures</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>toyota supra</td>
+        <td>150,000</td>
+        <td><img class="mypic" src="/html/img/toyota supra.jpeg" </td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>toyota high</td>
+        <td>50,000</td>
+        <td><img class="mypic" src="/html/img/toyota high.jpeg"</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>toyota corolla</td>
+        <td>20,000</td>
+        <td><img class="mypic" src="/html/img/toyota corolla.jpeg"</td>
+    </tr>
+
+</table>
+<ul>
+    <li>Egg</li>
+    <li>tomato</li>
+    <li>potato</li>
+    <li>onion</li>
+    <li>garlic</li>
+</ul>
+<ol>
+    <li>Egg</li>
+    <li>tomato</li>
+    <li>potato</li>
+    <li>onion</li>
+    <li>garlic</li>
+</ol>
+
+
+
+
+
+
+
+
+
+<style>
+ .table{
+     background-color: #005cbf;
+     width: 95%;
+     text-align: center;
+     margin-right: 20px;
+    }
+    .table{
+        margin: 15px;
+    }
+h3{
+    text-align: center;
+}
+    p{
+    background-color: #9fcdff;
+        width: 95%;
+        text-align: center;
+        /*float: right;*/
+    }
+    .mypic{
+        width: 20%;
+        height: 40px;
+    }
+
+    .colaction{
+        background-color: rgba(200, 35, 51, 0.73);
+        text-align: center;
+        font-family: "Verdana";
+    }
+ul{
+    border:2px dotted #0c5460;
+    background-color: #6f42c1;
+    width: 300px
+}
+
+
+
+
+
+</style>
+
+
+
+
+
+
+
+</body>
+<footer>
+
+</footer>
+</html>
