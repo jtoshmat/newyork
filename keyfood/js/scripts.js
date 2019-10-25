8011a81ce71e6bb1dfe29134c3fea82ff9bd1e94
MY-IT-EDUCATION.US <my-it-education.us@MYITEDUONUSsAir.home>
2019-10-25 16:09:12 -0400

    saving

--- keyfood/js/scripts.js
+++ keyfood/js/scripts.js
@@ -1,28 +1,19 @@
 $(function () {
-
-/*
-1) Click on items
-2) keep track of items quantities
-3) create print, total, grantotal, taxes
- */
-var id = '';
-$(".items").click(function () {
-    id = $(this).data('id');
-    $(this).addClass('items_clicked');
-    print(id);
+    var id = '';
+    $(".items").click(function () {
+        id = $(this).data('id');
+        $(this).addClass('items_clicked');
+        print(id);
+    });
 });
-
-
-});
-
 var total = 0;
 function print(id) {
     var tr_exists = $("#displaytable tr");
     total++;
-    if (tr_exists.hasClass("mytr"+id)){
+    if (tr_exists.hasClass("mytr" + id)) {
         //fergferfer
         return false;
-    }else{
+    } else {
         print_table(id, total);
         return false;
     }
@@ -30,42 +21,35 @@ function print(id) {
 }
 var itemcount = 0;
 function print_table(id, total) {
-    var product_name = $("#product_name"+id).val();
+    var product_name = $("#product_name" + id).val();
     itemcount++;
-    var tr = "          <tr class='mytr"+id+"'>\n" +
-        "                    <td>"+itemcount+"</td>\n" +
-        "                    <td>"+product_name+"</td>\n" +
-        "                    <td>"+total+"</td>\n" +
+    var tr = "          <tr class='mytr" + id + "'>\n" +
+        "                    <td>" + itemcount + "</td>\n" +
+        "                    <td>" + product_name + "</td>\n" +
+        "                    <td>" + total + "</td>\n" +
         "                    <td>$6.60</td>\n" +
-        "                    <td><button class='btnremove' data-id='"+id+"'>X</button></td>\n" +
+        "                    <td><button class='btnremove' data-id='" + id + "'>X</button></td>\n" +
         "                </tr>";
     $("#displaytable").append(tr);
 }
-
-$(document).on('click', '.btnremove', function(event) {
+$(document).on('click', '.btnremove', function (event) {
     var id = $(this).data('id');
     removeTableItem(id);
     return false;
 });
-
 function removeTableItem(id) {
-    $(".mytr"+id).remove();
-    $("#item"+id).removeClass("items_clicked");
+    $(".mytr" + id).remove();
+    $("#item" + id).removeClass("items_clicked");
 }
-
-
 function tax() {
 
 }
-
 function total() {
 
 }
-
 function grandtotal() {
 
 }
-
 function startover() {
 
 }
