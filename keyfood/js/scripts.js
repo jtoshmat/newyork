935a9446ff50033b0a7ce9515a5fc874acd928c3
sher <friend802000@gmail.com>
2019-10-25 17:27:56 -0400

    saving

--- keyfood/js/scripts.js
+++ keyfood/js/scripts.js
@@ -12,37 +12,6 @@ $(function () {
         print(id);
     });
 
-
-});
-
-var total = 0;
-
-function print(id) {
-    var tr_exists = $("#displaytable tr");
-    total++;
-    if (tr_exists.hasClass("mytr" + id)) {
-        //fergferfer
-        return false;
-    } else {
-        print_table(id, total);
-        return false;
-    }
-}
-
-var itemcount = 0;
-
-function print_table(id, total) {
-    var product_name = $("#product_name" + id).val();
-    itemcount++;
-    var tr = "          <tr class='mytr" + id + "'>\n" +
-        "                    <td>" + itemcount + "</td>\n" +
-        "                    <td>" + product_name + "</td>\n" +
-        "                    <td><button id='minus'>-</button>" + total + "<button id='add'>+</button></td>\n" +
-        "                    <td>$6.60</td>\n" +
-        "                    <td><button class='btnremove' data-id='" + id + "'><span class='fa fa-trash'></span></button></td>\n" +
-        "                </tr>";
-
-    $("#displaytable").append(tr);
     $('.btnremove').css({
         'background': '#bdb9cb',
         'color': '#B1040D',
@@ -61,8 +30,36 @@ function print_table(id, total) {
         'border': '#bdb9cb',
         'font-size':'20px'
     });
-}
 
+
+});
+var total = 0;
+function print(id) {
+    var tr_exists = $("#displaytable tr");
+    total++;
+    if (tr_exists.hasClass("mytr" + id)) {
+        //fergferfer
+        return false;
+    } else {
+        print_table(id, total);
+        return false;
+    }
+}
+var itemcount = 0;
+function print_table(id, total) {
+    var product_name = $("#product_name" + id).val();
+    itemcount++;
+    var tr = "          <tr class='mytr" + id + "'>\n" +
+        "                    <td>" + itemcount + "</td>\n" +
+        "                    <td>" + product_name + "</td>\n" +
+        "                    <td><button id='minus'>-</button>" + total + "<button id='add'>+</button></td>\n" +
+        "                    <td>$6.60</td>\n" +
+        "                    <td><button class='btnremove' data-id='" + id + "'><span class='fa fa-trash'></span></button></td>\n" +
+        "                </tr>";
+
+    $("#displaytable").append(tr);
+
+}
 $(document).on('click', '.btnremove', function (event) {
     var id = $(this).data('id');
     removeTableItem(id);
@@ -71,15 +68,10 @@ $(document).on('click', '.btnremove', function (event) {
 function addAndSubstract() {
 
 }
-
-
 function removeTableItem(id) {
     $(".mytr" + id).remove();
     $("#item" + id).removeClass("items_clicked");
 }
-
-
-
 function tax() {
 
 }
