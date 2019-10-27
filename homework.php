15a335f74536644dfdd12bfe20b38f1ff95d15f2
Jon Toshmatov <jontoshmatov@yahoo.com>
2019-10-27 19:16:02 -0400

    saving regex

--- homework.php
+++ homework.php
@@ -1,16 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>REGEX</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
 <?php
-$ssn = ["123-34-5678", "456-34-4356","123-345-3456","sdfsdf-4234-sdfsdf","12-3344-12234","cd23-qw222-4444"];
-$ssn2 = ["323-44-5678", "432*34-4356","123-686-3456","sdfsdf111ddf-sdfsdf","12+23123-12234","123-45-6789a"];
-$numbers = array_merge($ssn, $ssn2);
-$y =0 ;
-foreach ($numbers as $num=>$item){
-    $y++;
-    $found = preg_match("/^[0-9]{3}-[0-9]{2}-[0-9]{4}$/", $item, $match);
-    echo "$y) ";
-    echo $item.": ";
-    echo $found;
-    echo "<hr>";
+$parms = $_POST;
+$dob = null;
+if (isset($parms['dob'])){
+    $dob = $parms['dob'];
+}
+$ssn = null;
+if (isset($parms['ssn'])){
+    $ssn = $parms['ssn'];
+}
+$phone = null;
+if (isset($parms['phone'])){
+    $phone = $parms['phone'];
+}
+
+
+//07-23-1990
+$found = preg_match("#[0-9]{2}-[0-9]{2}-[0-9]{4}$#", $dob, $match);
+if (!$found){
+    $error = "Your DOB is not in correct format";
+}
+
+//123-45-6789
+$found = preg_match("#[0-9]{3}-[0-9]{2}-[0-9]{4}$#", $ssn, $match);
+if (!$found){
+    $error = "Your SSN is not in correct format";
+}
+
+//123-45-6789
+$found = preg_match("#\([0-9]{3}\)-[0-9]{3}-[0-9a-zA-Z]{4}$#", $phone, $match);
+if (!$found){
+    $error = "Your Phone is not in correct format";
 }
 ?>
+<div class="container-fluid">
 
+    <p>Contact Form</p>
+    <?php
+    if (isset($error)) {
+        ?>
+        <div class="alert alert-danger"><?=$error?></div>
+        <?php
+    }
+    ?>
 
+    <form method="post">
+        <p>DOB: <input value="<?=$dob?>" name="dob" type="text" placeholder="<?=date('m-d-Y')?>"></p>
+        <p>SSN: <input value="<?=$ssn?>" name="ssn" type="text" placeholder="123-45-6789"></p>
+        <p>Phone: <input value="<?=$phone?>" name="phone" type="text" placeholder="347-576-4951"></p>
+        <p><button class="btn btn-primary">Submit</button></p>
+
+    </form>
+
+</div>
+</body>
+</html>
