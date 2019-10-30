d029929777a56a8edb05e324fb17b214ec0c4e1d
Jon Toshmatov <jontoshmatov@yahoo.com>
2019-10-30 19:31:14 -0400

    saving

--- wallstreet/index.php
+++ wallstreet/index.php
@@ -1,105 +1,17 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>Welcome to Wall Street!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML TABLE EXERCISES</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
-
 <body>
+<div class="container-fluid">
 
 
-<ul>
-    <li>Tashkent
-        <ol>
-            <li>Mirzo Uluqbek</li>
-            <li>Chilanzar</li>
-            <li>Shayhontur</li>
-            <li>Hamza</li>
-            <li>Yunosobod</li>
-        </ol>
-    </li>
-    <li>Kiev</li>
-    <li>Moscow
-        <ol>
-            <li>Kuzminki</li>
-            <li>Tsaritsino</li>
-            <li>Tverskoy</li>
-            <li>Izmaylovo</li>
-            <li>Chertanovo</li>
-        </ol>
-    </li>
-    <li>New York</li>
-    <li>Paris</li>
-</ul>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<h1>Welcome to Wall Street page!</h1>
-
-<p>
-    Hello, My name is Jon and I love programming.
-    <br>
-    <br>
-    I live in Brooklyn and in my free I enjoy walking in the park.
-</p>
-
-<p>
-    I live in Brooklyn and in my free I enjoy walking in the park.
-</p>
-
-<p>
-    <img title="Yaaay, mountains ROCK!" alt="This is a picture of a mountain" width="500px" src="img/mountainscene.jpg">
-</p>
-
-<p>
-    <a title="Click to go to Google website" target="_blank" href="http://google.com">Google</a>
-</p>
-
-<p>
-    <a href="mailto:jontoshmatov@yahoo.com?subject=Hello&body=This is a friendly reminder">Send Email</a>
-</p>
-
-
-
-
+</div>
 </body>
 </html>
