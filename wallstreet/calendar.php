574bee7623c5cf5f215dbeb2c52fd43a0db5ace7
Rustam Mirakbarov <rustammirakbarov@Rustams-MBP.home>
2019-11-12 19:33:13 -0500

    I am Saving

---
+++ wallstreet/calendar.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <script src="../js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<table class="container-fluid">
+    <tr>
+    <tr>
+        <th>November</th>
+    </tr>
+    <tr>
+        <td class="weekdays">m t w th f s s</td>
+    </tr>
+    <tr>
+        <td>1 2 3 4 5 6 7</td>
+    </tr>
+    <tr>
+        <td>8 9 10 11 12 13 14</td>
+    </tr>
+    <tr>
+        <td>15 16 17 18 19 20 21</td>
+    </tr>
+    <tr>
+        <td>22 23 24 25 26 27 28</td>
+    </tr>
+    <tr>
+        <td>29 30</td>
+    </tr>
+
+
+</table>
+<style>
+    .weekdays
+    {
+        background-color: #302af5;
+    }
+    th
+    {
+        background-color: grey;
+        text-align: center;
+    }
+
+
+    .container-fluid
+    {
+        width: 400px;
+    }
+    table
+
+    {
+        text-align: center;
+        background-color: #b3d7ff;
+        border: 1px solid #000;
+        border-collapse: collapse
+
+
+    }
+    tr, td
+    {
+        padding: 10px;
+    }
+
+
+</style>
+</body>
+</html>
