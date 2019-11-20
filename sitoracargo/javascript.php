88e4b1fc731744077066aff8e271abd606476122
Admin <jontoshmatov@yahoo.com>
2019-11-20 14:47:22 -0500

    saving Lissa code

---
+++ sitoracargo/javascript.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Javascrips Exercises</title>
+
+</head>
+<body>
+
+<div id="sandikcha">
+
+
+
+</div>
+<style>
+    .posudalar{
+        background-color: #a50007;
+        color: #ffeb37;
+        padding:5px;
+    }
+</style>
+<script src="js/scripts.js"></script>
+</body>
+</html>
