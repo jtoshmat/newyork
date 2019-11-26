f3a19ebcadafef93c34e18ab5bab4062b9c9b2a9
Admin <jontoshmatov@yahoo.com>
2019-11-26 13:55:19 -0500

    saving my calculator js code for Sitora calculator

--- sitoracargo/javascript.php
+++ sitoracargo/javascript.php
@@ -5,53 +5,52 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Javascrips Exercises</title>
+    <title>My First Basic Calculator</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
-
+    <script src="../js/jquery-3.4.1.min.js"></script>
 </head>
 <body>
-
  <div class="container-fluid">
+     <div style="height: 50px;"></div>
      <div id="mycalculator">
         <table class="table">
             <tr>
                 <td class="display" colspan="4">0</td>
             </tr>
             <tr>
-                <td>AC</td>
-                <td>+/-</td>
-                <td>%</td>
-                <td>/</td>
+                <td class="operator">AC</td>
+                <td class="operator">+/-</td>
+                <td class="operator">%</td>
+                <td class="operator">÷</td>
             </tr>
             <tr>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-                <td>X</td>
+                <td class="numbers">7</td>
+                <td class="numbers">8</td>
+                <td class="numbers">9</td>
+                <td class="operator">×</td>
             </tr>
             <tr>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>-</td>
+                <td class="numbers">4</td>
+                <td class="numbers">5</td>
+                <td class="numbers">6</td>
+                <td class="operator">-</td>
             </tr>
 
             <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>+</td>
+                <td class="numbers">1</td>
+                <td class="numbers">2</td>
+                <td class="numbers">3</td>
+                <td class="operator">+</td>
             </tr>
             <tr>
-                <td colspan="2">0</td>
-                <td>.</td>
-                <td>=</td>
+                <td colspan="2" class="numbers">0</td>
+                <td class="operator">.</td>
+                <td id="result">=</td>
             </tr>
         </table>
      </div>
 
  </div>
-
 <style>
 
     tr td:last-child{
@@ -93,6 +92,7 @@
     border-radius: 10px;
     border:1px solid black;
     box-shadow: #cacaca 5px 5px 5px 5px;
+    margin: auto;
 }
 </style>
 <script src="js/scripts.js"></script>
