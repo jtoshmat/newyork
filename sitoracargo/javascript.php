af37d7b1b58b75ddd7e41bdd6a47457013a4ea32
Admin <jontoshmatov@yahoo.com>
2019-12-01 12:55:05 -0500

    saving

--- sitoracargo/javascript.php
+++ sitoracargo/javascript.php
@@ -14,7 +14,7 @@
    <form id="myform" method="post">
        <p>My Basic Currency Calculator to USD</p>
        <p>
-           Currency: <input name="currency" placeholder="Enter your currency amount" type="text" id="currency_amount">
+           Currency: <input name="currency" placeholder="Enter your currency amount" type="text" id="currency_amount">$
            <select name="currency_types" id="currency_types">
                <option value="rub">RUB</option>
                <option value="uzs">UZS</option>
@@ -40,20 +40,27 @@
         var currency_types = $("#currency_types").val();
         var output = '';
         var money_type = '';
+        $(".mybox").removeClass("rus_box");
+        $(".mybox").removeClass("uzb_box");
+        $(".mybox").removeClass("jpn_box");
+
         switch (currency_types) {
             case 'rub':
                 output = currency_amount * 64.30;
                 money_type = 'Rossiskiy Ruble';
+                $(".mybox").addClass("rus_box");
                 break;
 
             case 'uzs':
                 output = currency_amount * 9491.18;
                 money_type = 'Uzbek SOM';
+                $(".mybox").addClass("uzb_box");
                 break;
 
             case 'yen':
                 output = currency_amount * 109.48;
                 money_type = 'Japanese YEN';
+                $(".mybox").addClass("jpn_box");
                 break;
 
             default:
@@ -68,12 +75,16 @@
 </script>
 
 <style>
+    #currency_amount{
+        width:70px;
+    }
     .mybox{
         background-color: #520006;
         color: #ffe14a;
         width: 500px;
         height: 200px;
         margin: auto;
+        border:1px solid black;
     }
     #myresults{
         background-color: #f5cbcd;
@@ -82,6 +93,27 @@
         padding:5px;
         display: none;
     }
+
+    .uzb_box{
+        background-image: url("img/uzb_bg.jpg");
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        color:darkblue;
+    }
+
+    .rus_box{
+        background-image: url("img/rus_bg.jpg");
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        color:darkblue;
+    }
+    .jpn_box{
+        background-image: url("img/jpn_bg.jpg");
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        color:darkblue;
+    }
+
 </style>
 </body>
 </html>
