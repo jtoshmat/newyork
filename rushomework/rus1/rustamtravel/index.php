b12425b77f1a1e92d05b01b23523feaff361d233
Rustam Mirakbarov <rustammirakbarov@Rustams-MBP.home>
2019-12-06 22:49:14 -0500

    saving my work

---
+++ rushomework/rus1/rustamtravel/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Travel</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../../css/bootstrap.css">
+    <script src="../../../js/jquery-3.4.1.min.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+    <div id="topheader">
+        <h1 class="header1">Rustam Travel</h1>
+        <div class="options">
+            <button id="USA">USA</button>
+            <button id="TURKEY">TURKEY</button>
+            <button id="UZB">UZB</button>
+            <button id="UAE">UAE</button>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="clearfix"></div>
+
+    <div id="presentation_window">
+        <div class="leftwindow">
+            <img alt="The img of city" title="Click on the large to display it larger" class="leftimg" id="usimg" src="pics/usa1.jpg">
+            <br>
+            <img alt="The img of city" title="Click on the large to display it larger" class="leftimg" id="usimg1" src="pics/usa1.jpg">
+            <br>
+            <img alt="The img of city" title="Click on the large to display it larger" class="leftimg" id="usimg2" src="pics/usa1.jpg">
+        </div>
+        <div class="rightwindow">
+            <img class="rightimgs" id="usaimg3" src="pics/ny1.jpg">
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+
+    <button class="button2">CALL US</button>
+
+</div>
+
+</body>
+</html>
