25a68854e07d1442ef3f1e9883f3e202c0a920be
lissa uzbek <mukhlissa@gmail.com>
2020-01-10 10:29:34 -0500

    saving lisa's code

---
+++ mukhlissa/homeworks/welcome.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="mywrapper">
+        <p class="logo" style="color:white; text-align: left; font-style: italic; font-family: SignPainter;">Belle for Elle</p>
+        <div class="container">
+            <button>Home</button>
+            <button>About</button>
+            <button>Rates</button>
+            <button>HairStyles</button>
+            <button>Makeup</button>
+            <button>Contact</button>
+        </div>
+        <div class="middle_text">
+            <div class="empty_container"></div>
+            <h1 style="text-align: center;">Mobile Beauty Studio in DMV</h1>
+            <h3 style="text-align: center">Hair and Makeup Services at Your Place for Your Convenience</h3>
+        </div>
+        <div class="bottom_buttons">
+            <button>Learn More</button>
+            <button>Request Free Phone Consultation</button>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h2 style="text-align: center;color:mediumpurple">Hair Styling</h2>
+                <h4 style="text-align: center">Professional Hair Stylist Services for Fashion, Photography, Bridal, Entertainment, and Special Events</h4>
+                <h4 style="text-align: center">State Licensed, trained by Famous European Hair Artist Georgy Kot</h4>
+            </div>
+            <div class="col-sm">
+                <h2 style="text-align: center;color:mediumpurple">Makeup</h2>
+                <h4 style="text-align: center">High Quality Makeup Services from Natural Makeup to HD or Smokey</h4>
+                <h4 style="text-align: center">State Licensed, trained by Famous European Makeup Artist Goar Avertisyan</h4>
+
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+
+    .col-sm{
+        border:1px solid black;
+        height:200px;
+        margin-right: 5px;
+    }
+
+    .container-fluid{
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .bottom_buttons{
+        text-align: center;
+        margin-top:150px;
+    }
+    .container{
+    text-align: center;
+    margin-bottom: 100px;
+    }
+
+
+    .mywrapper {
+        background-image: url("../img/belleforelle1.jpg");
+        background-color: white;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        height: 900px;
+
+    }
+
+
+    button:hover{
+        color:purple;
+        cursor: pointer;
+    }
+    .middle_text{
+        margin-top:400px;
+    }
+    .logo {
+        width: 250px;
+        height: 300px;
+    }
+.empty_container {
+    height: 20px;
+    width:150px;
+}
+</style>
+</body>
+</html>
