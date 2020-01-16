de0ed14b2524617f0e4d4f0b77133306dc1dd4f3
shamsher <shamsherxon@gmail.com>
2020-01-16 10:23:45 -0500

    Lesson One, Two

---
+++ shakhnoza/travel/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Travel agency</title>
+</head>
+<body>
+<div class="container-fluid"></div>
+         <div class="topdiv">
+             Top Div
+         </div>
+<div class="middledic">
+    Middle Div
+</div>
+<div class="bottomdiv">
+    Bottom Div
+</div>
+
+</body>
+</html>
