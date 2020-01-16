9e2a067ec78ef36dd313309199a2ee675cd2dc0a
Barno <barnom75@gmail.com>
2020-01-15 16:25:07 -0800

    saving my code

---
+++ barno/tuesday.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html xmlns:text-align="http://www.w3.org/1999/html">
+<head>
+    <title>This is BTS</title>
+</head>
+h
+<center><h1 style="color:white;">✧･ﾟ: *✧･ﾟ:* BTS *:･ﾟ✧*:･ﾟ✧</h1></center>
+
+<center><img src="https://dbkpop.com/wp-content/uploads/2019/03/bts_map_of_the_soul_persona_concept_all_1.jpg" width="770" height="500"</center>
+<p style="color:#ac00e6;">map of the soul : persona (photoshoot).</p>
+
+<br>
+<center><img src="https://ibighit.com/bts/images/bts/discography/map_of_the_soul-persona/version04-06.jpg" height="340"</center>
+<p style="color:red;">.｡.:*☆Kim Namjoon - known as RM or Rapmon, the leader of the group.｡.:*☆</p>
+
+<center><img src="https://ibighit.com/bts/images/bts/discography/map_of_the_soul-persona/version04-04.jpg" height="340" </center>
+<p style="color:#ff9999;">.｡.:*☆Kim SeokJin - the oldest of the group, the main vocalist and the hyung..｡.:*☆</p>
+
+<center><img src="https://i.pinimg.com/originals/bc/01/b3/bc01b3819d6fc406e7f2134f0361433e.jpg" height="340" </center>
+<p style="color:#c266ff;">.｡.:*☆Min Yoongi - also known as suga, the rapper..｡.:*☆</p>
+
+<center><img src="https://ibighit.com/bts/images/bts/discography/map_of_the_soul-persona/version04-02.jpg" height="340"</center>
+<p style="color:darkred;">.｡.:*☆Jung Hoseok - also known as J-Hope, the groups rapper, main dancer and the sunshine :).｡.:*☆</p>
+
+<center><img src="https://dbkpop.com/wp-content/uploads/2019/04/bts_map_of_the_soul_persona_concept_Jimin_3.jpg" height="340"</center>
+<p style="color:orangered;">.｡.:*☆Park Jimin - also one of the main vocalists because of his high pitched and very stable voice..｡.:*☆</p>
+
+<center><img src="http://images6.fanpop.com/image/photos/42700000/MAP-OF-THE-SOUL-PERSONA-Concept-Photo-version-v-bts-42727847-1024-683.jpg" height="340"</center>
+<p style="color:#3385ff;">.｡.:*☆Kim Taehyung - known as V, also one of the main vocalists, very popular for his deep
+    <br> voice. Was nominated as the Worlds Most beautiful man in 2017..｡.:*☆</p>
+
+<center><img src="http://images6.fanpop.com/image/photos/42700000/MAP-OF-THE-SOUL-PERSONA-Concept-Photo-version-jungkook-bts-42727852-1024-683.jpg" height="340"</center>
+<p style="color:#ff6666;">.｡.:*☆Jeon Jungkook - main dancer, main vocalist and the Golden Maknae.｡.:*☆</p>
+
+
+
+</>
+</html>
