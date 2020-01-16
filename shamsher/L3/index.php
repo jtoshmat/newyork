b632a1e74eb50edebcf9927b92a5b5ab531a96af
shamsher <shamsherxon@gmail.com>
2020-01-16 15:42:13 -0500

    expedia

--- shamsher/L3/index.php
+++ shamsher/L3/index.php
@@ -5,10 +5,56 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Expedia Travel</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="topmenu">
+    topmenu
+</div>
+<div class="mainmenu">
+    <div style="height: 30px"></div>
+    <div class="serchbox">
 
+    </div>
+
+</div>
+<div class="footer">
+    footer
+</div>
+
+<style>
+    .topmenu {
+        width: 100%;
+        height: 150px;
+        background-color: #FFC107;
+
+    }
+    .mainmenu{
+        width: 100%;
+        height: 500px;
+        background-size:  100% 100%;
+        background-image: url("https://wordpress-308562-967316.cloudwaysapps.com/wp-content/uploads/2014/07/Maldives.png");
+        background-repeat: no-repeat;
+
+
+    }
+    .serchbox{
+        width: 75%;
+        height: 432px;
+        background-color: black;
+        margin: auto;
+        border-radius: 10px;
+        opacity: 0.7;
+
+    }
+
+    .footer{
+        width: 100%;
+        height: 150px;
+        background-color: red;
+
+    }
+</style>
 </body>
 </html>
