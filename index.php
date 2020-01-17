b24b74b7e8636278bb1b5ebbde81228e2dd2e20e
Admin <jontoshmatov@yahoo.com>
2020-01-17 11:20:54 -0500

    saving Lissa's code

--- index.php
+++ index.php
@@ -1,8 +1,54 @@
-<?php
-function inc($a){
-    return $a++;
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+    html
+    head
+    title
+    body
+    h1
+    p
+    div
+    img
+    figure
+    figureacaption
+    table
+    dialog
+    span
+    form
+    select -> optgroup -> label
+    fieldset
+    legend
+    hr
+    br
+    progress
+    meter
+    details
+    summary
+    ul
+    nav
+    a
+    article
+    strong
+    iframe
+    frameset
+    frame
+    audio
+    video
+    section
+    sup
+    sub
+    strong
 
-$jon = inc(5);
-echo $jon;
+</pre>
 
+
+</body>
+</html>
