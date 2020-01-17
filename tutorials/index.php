09fa35f6d47cdf8e0f856d0210668bdac8bfe0fe
Admin <jontoshmatov@yahoo.com>
2020-01-17 16:06:24 -0500

    saving barcelona code

---
+++ tutorials/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Jon Toshmatov">
+    <meta name="keywords" content="HTML, CSS, Javascript tutorials">
+    <meta name="description" content="This site is about online tutorials">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is HTML tutorial by Jon Toshmatov</title>
+</head>
+<body>
+<h1>HTML Tutorialiga xush kelibsiz!</h1>
+</body>
+</html>
