d7b843ad9d1146e41d6fa2f90d842d77124f347b
Fotima Maks <Maks@Fotimas-MBP.home>
2020-01-21 19:33:47 -0500

    saving

---
+++ practice_flighthub/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Bensonhurst TaeKwonDo Center</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<div class="container-fluid">
+<div class="header">
+    <ul>
+        <li><a href="a">Home</a></li>
+        <li><a href="a">About Us</a></li>
+        <li><a href="a">Programs</a></li>
+        <li><a href="a">Contact Us</a></li>
+    </ul>
+</div>
+<div class="topdiv">Top Div</div>
+<div class="middlediv">Middle Div</div>
+<div class="bottomdiv">Bottom Div</div>
+</div>
+</body>
+</html>
+
