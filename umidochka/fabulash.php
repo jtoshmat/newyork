9c1a63fec484bebd03584163a6583b1d188a3180
Umidochka <48989483+Umidochka@users.noreply.github.com>
2020-01-21 23:33:47 -0500

    saving

--- umidochka/fabulash.php
+++ umidochka/fabulash.php
@@ -14,7 +14,7 @@
 <div class="mybody">
     <div class="mynavbar">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Home</a>
+            <a class="navbar-brand" href="#"><img src="" </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -402,7 +402,7 @@
         }
 
        .parallax{
-           background-image: url("../umidochka/img/stick1.jpg");
+           background-image: url("../umidochka/img/mywall.jpg");
            min-height: 700px;
            background-attachment: fixed;
            background-position: center;
