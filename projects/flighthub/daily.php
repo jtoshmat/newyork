f93e4aa83cc456a80e50c8b594d8ee4bbe71eaf6
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-01-25 18:16:31 -0500

    I created the top portion of the flighthub for the Apple team

--- projects/flighthub/daily.php
+++ projects/flighthub/daily.php
@@ -1,7 +1,43 @@
-<?php
-function validatePIN($pin) {
-    return preg_match('#(^[0-9]{4}$)|(^[0-9]{6}$)#',$pin);
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+<div class="mybox">
+    <div class="top_div">
+        <nav class="nav1">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Our Products</a>
+            <a href="#">Others</a>
+        </nav>
+    </div>
+    <div class="logo_div">
+            <div>
+                <img id="logo" src="img/logo.png">
+            </div>
+            <div>
+                <nav class="nav2">
+                    <a href="#">Flights</a>
+                    <a href="#">Hotels</a>
+                    <a href="#">Cars</a>
+                    <a href="#">Travel Guides</a>
+                    <a href="#">My Trips</a>
+                </nav>
 
-$jon = validatePIN("12345s");
-var_dump($jon);
+            </div>
+            <div>
+                <span>CALL US TOLL FREE 24/7</span> <br>
+                <span>1-800-555-1234</span>
+            </div>
+    </div>
+</div>
+</body>
+</html>
