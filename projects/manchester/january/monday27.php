bc00a83a871e68ce2d89882b024d322ebb24b901
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-01-27 13:05:40 -0500

    1:05

--- projects/manchester/january/monday27.php
+++ projects/manchester/january/monday27.php
@@ -1,3 +1,9 @@
 <?php
+//concatination
+//concat
 
-//Deleted
+$first_name = 'Jon';
+$middle_name = 'Halimovich';
+$last_name = 'Toshmatov';
+
+echo $first_name.' <strong> '.$middle_name.' </strong> '.$last_name;
