72723e6057248b97137d7f876a1a0e1e641a464a
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-01-29 13:35:35 -0500

    saving lottery

--- projects/manchester/january/wednesday29.php
+++ projects/manchester/january/wednesday29.php
@@ -1,34 +1,11 @@
 <?php
-//Arrays exercises for beginners
-$countries = [
-        'England'=> ['Manchester_United'=> ['D.Gea','V.Lindelof','G.Maguaer','L.Shaw','A.V.Bissaka','P.Pogba','Fred','D.James','N.Matic','M.Rashford','M.Greenwood'],
-        'Liverpool','Man City',],
-    'Italy' => ['Inter','Milan','fav'=>'Juventus'],
-    'France' => ['PSJ'],
-    ['Pakhtakor'],
-    'Spain' => ['Real Madrid','Barcelona'],
-    'Germany' => ['Bayern Munich'],
-    'first' => 'FIFA',
-    'second' => 'UEFA',
-    2020 => ['US' => [2015,2016,2017,2018,2019,]]];
+echo "<pre>";
+$cars = ['Audi','Honda','Alfa Romer','Nissan'];
 
-//Question: Arraylarni valuesini $ bilan script qilish mumkinmi?
-$clubs = ['Manchester','Real Madrid','Barcelona','Liverpool','Man City','Inter','Milan','Juventus',
-'PSJ','Pakhtakor','Bavariya'];
-
-unset($countries['Uzbekistan']);
+list($audi, $japan, $italian) = $cars;
 
 
-$countries[0] = ['Dinami_Moskva','Zenit','Lokomotiv'];
 
-//$countries['Uzbekistan'] = 'Russian'; //Savol: Urniga qanaqa qilib quyiladi Russia.
-echo '<pre>';
-//print_r($countries['England']);
-
-//print_r($countries[2020]['US'][4]);
-
-//echo "My favorite club is " . $countries['England']['Manchester_United'][4];
+echo "I drove $audi and $italian but never like $japan";
 
 
-//echo $countries['England']['Manchester_United'][0].' '. $clubs[3];
-print_r($countries);
