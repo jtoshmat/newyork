89c2983aa894c8f265f616ec989980c535ca712b
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-01-29 17:13:11 -0500

    saving

--- projects/apple/jon2.php
+++ projects/apple/jon2.php
@@ -5,29 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>January 29, 2020</title>
+    <title>Image exercises</title>
     <link rel="stylesheet" href="../../css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/jon.css">
 </head>
 <body>
-
 <div class="container-fluid">
     <div class="mybox">
-
-        <div class="logodiv mydivs">
-            <img src="img/flight_logo.png">
+        <div class="logodiv">
+            <img class="logo" src="img/flightlogo.png">
         </div>
-        <div class="textdiv mydivs">
+        <div class="menudiv">
             <nav>
                 <a href="#">Home</a>
                 <a href="#">About Us</a>
                 <a href="#">Our Services</a>
                 <a href="#">Contact Us</a>
             </nav>
+            <div class="contactnumbers">
+                <span>Call Us Today</span><br>
+                <span>(800) 123-3456</span>
+            </div>
         </div>
-
     </div>
 </div>
-
 </body>
 </html>
