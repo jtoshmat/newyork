b5242a1b549ef138eefac774d0d52fdea7eb944b
Fotima Maks <Maks@Fotimas-MBP.home>
2020-02-01 19:41:17 -0500

    saving

---
+++ projects/microsoft/feb1.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="css/feb1.css">
+</head>
+<body>
+<div class="container-fluid">
+    <table class="table">
+        <tr>
+            <td>ID</td>
+            <td>Student</td>
+            <td>Age</td>
+            <td>Images</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Alex</td>
+            <td>7</td>
+            <td><img class="img" src="../microsoft/img/fev1_boy.jpeg"</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Emma</td>
+            <td>6</td>
+            <td><img class="img" src="../microsoft/img/7219379-portrait-of-a-cute-little-girl-outside.jpg"</td>
+        </tr>
+
+    </table>
+</div>
+<div class="form">
+    <form>
+        <div style="height:100px">
+            <img class="logo" src="../microsoft/img/login2.png">
+        </div>
+        <p>
+            <label>First Name</label>
+            <input type="text" name="first_name " placeholder="First Name">
+        </p>
+        <p>
+            <label>Last Name</label>
+            <input type="text" name="last_name" placeholder="Last Name">
+        </p>
+        <p>
+            <label>Phone Number</label>
+            <input type="tel" name="phone_number" placeholder="Phone Number">
+        </p>
+        <p>
+            <label>Email</label>
+            <input type="text" name="email_address" placeholder="Email Address">
+        </p>
+        <button>Reset</button>
+        <button>Submit</button>
+    </form>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ projects/microsoft/magicblanket.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="css/magicblanket.css">
+</head>
+<body>
+<div class="box">
+    <div style="height:100px"></div>
+    <div class="whitebox">
+        <div class="leftdiv boxes">
+            <div class="first"><img src="img/blanket1.png"></div>
+            <div class="first"><img src="img/blanket2.png"></div>
+        </div>
+        <div class="middlediv boxs">
+            <div class="second"><img src="img/blanket3.jpeg"</div>
+            <div class="second"><img src="img/blanket4.jpeg"</div>
+        </div>
+        <div class="rightdiv boxes">
+            hjsjsjsjsjs
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
