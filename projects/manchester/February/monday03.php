d46fdbf8b751488ee9c79fb9c3283917cfe5b6d9
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-03 12:25:31 -0500

    Saving Manchester's feb 03 part 1 code

---
+++ projects/manchester/February/monday03.php
@@ -0,0 +1,133 @@
+<?php
+
+define('COUNTRY','Spain');
+define('DETAILFILE', __FILE__.':');
+
+
+
+$countries = ['Russia','USA','Uzbekistan','Canada','Ukraine', 'Spain'];
+
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        error_log("This is an error log in line: ".DETAILFILE.__LINE__);
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+echo "<hr>";
+
+foreach($countries as $country){
+    if ($country == COUNTRY){
+        echo "$country - I LOVE ". COUNTRY;
+        echo "<hr>";
+    }else{
+        echo "$country - That is an awesome contry too";
+        echo "<hr>";
+    }
+}
+
+error_log("This is the EOF: ".DETAILFILE.__LINE__);
