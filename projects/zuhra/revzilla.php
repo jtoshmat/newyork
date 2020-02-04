d82dbbe13d9d3c0eec0cdf923456113e726cc889
zuhra <zuhramaksudi@gmail.com>
2020-02-04 18:11:15 -0500

    saving

---
+++ projects/zuhra/revzilla.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Zuhra Maksudi">
+    <meta name="description" content="This is a copy of revzilla">
+    <meta name="keyword" content="revzilla, online shop, motorcycles, gears, HD Videos">
+    <title>Motorcycle Gear, Free Shipping & HD Video Review Online -RevZilla</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="css/revzilla.css">
+</head>
+<body>
+<div class="container-fluid" style="padding: 0 0">
+    <div class="header" style="width: 100%; height: 84px">
+        <nav class="nav1">
+            <a href="https://www.revzilla.com/#" title="Navigation">
+                <img class="bar" src="img/bar.png" alt="bar">
+            </a>
+            <a target="_blank" href="https://www.revzilla.com/">
+                <img class="img_logo" src="img/revzilla.png" alt="revzilla_logo"></a>
+            <a href="https://www.revzilla.com/cart" title="Cart">
+                <img class="cart" src="img/cart_edited.jpg" alt="cart"></a>
+            <a class="phone" title="Phone" href="tel:+8777929455">
+                <i class="material-icons">phone</i>
+            </a>
+        </nav>
+    </div>
+    <div class="searchbox">
+            <form>
+                <input class="search" type="text" name="firstname">
+        </form>
+    </div>
+    <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+            <li data-target="#demo" data-slide-to="3"></li>
+            <li data-target="#demo" data-slide-to="4"></li>
+        </ul>
+        <div class="inner">
+            <div class="item active">
+                <img src="https://www.revzilla.com/asset_instance_attachments/30547/30547_1579620309_desktop_1x.jpg" alt="slide1">
+            </div>
+            <div class="item">
+                <img src="https://www.revzilla.com/asset_instance_attachments/28970/28970_1572016002_desktop_1x.jpg" alt="slide2">
+            </div>
+            <div class="item">
+                <img src="https://www.revzilla.com/asset_instance_attachments/30532/30532_1579286996_desktop_1x.jpg" alt="slide3">
+            </div>
+            <div class="item">
+                <img src="https://www.revzilla.com/asset_instance_attachments/30520/30520_1579272296_desktop_1x.jpg" alt="slide4">
+            </div>
+            <div class="item">
+                <img src="https://www.revzilla.com/asset_instance_attachments/30605/30605_1580504880_desktop_1x.jpg" alt="slide5">
+            </div>
+        </div>
+    </div>
+</div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $("#carousel").carousel();
+</script>
+</body>
+</html>
