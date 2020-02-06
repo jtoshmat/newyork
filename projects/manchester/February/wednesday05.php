9981babe407fe270c26c55417a466cdcb0e380b9
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-05 21:11:26 -0500

    saving google team Wednesday work

--- projects/manchester/February/wednesday05.php
+++ projects/manchester/February/wednesday05.php
@@ -5,55 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wednesday, February 05, 2020</title>
-    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <title>Form GET</title>
 </head>
 <body>
-<?php
-//Global variables
-//$_GET --> In Progress
 
-//$_POST
-//$_REQUEST
-//$_SESSION
-//$_COOKIE
-//$GLOBALS
-
-
-$mujozh= [
-    'ismi' => 'Umida',
-    'yoshi' => 18,
-   // 'yashashi' => 'Samarkand',
-    'malumoti' => 'Certified'
-];
-?>
+<form method="post" action="monday10.php">
+    <p>
+        <label>Username: </label>
+        <input required="required" type="email" name="email" value="" placeholder="Your Email Address">
+    </p>
+    <p>
+        <label>Last Name: </label>
+        <input type="password" name="password" value="" placeholder="Your Password">
+    </p>
 
 
-<h3>Welcome <?=$mujozh['ismi']?>!!!</h3>
+    <p>
+        <button type="reset">Reset</button>
+        <button type="submit">Login</button>
+    </p>
+</form>
 
-<p>
-    I would like to introduce our special guest today, his name is <?=$mujozh['ismi']?> and he is very talented and very energetic member of our organization.
-    <br>
-    He is only <?=$mujozh['yoshi']?> years old and he already earned his <?=$mujozh['malumoti']?> degree and he lives in Mars.
+<a href="monday10.php?ismi=Jon&familiyasi=toshmatov&mutahasisligi=IT">Monday</a>
 
-    <br>
-
-    Please welcome him/her to our team!
-
-    <br>
-
-    WOHOOOOOOOO!!!!
-</p>
-
-
-
-Go to <a href="monday10.php">Monday</a>
-<style>
-    body{
-        background-color: #6dcae5;
-    }
-</style>
 </body>
 </html>
-
-
