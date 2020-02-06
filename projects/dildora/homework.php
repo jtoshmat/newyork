43d0b81fd0d1281d425f5dfde3dca237ba1b0286
Dildora778 <rakhmonovadildora700@gmail.com>
2020-02-05 21:19:39 -0500

    saving

---
+++ projects/dildora/homework.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>homework</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="css/stylehome.css">
+
+</head>
+<body>
+<div class="container-fluid">
+    <div style="height: 100px"></div>
+ <div class="mybox">
+     <div class="header">
+         <div class="logo">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtyM49ZLoujNbjF1IMhbVaSwPAseFqKfCBfM_JDw6lQEJEgtDv">
+
+         </div>
+     </div>
+     <div class="mycheck">
+         <p1>Special savings<br>just for you</p1>
+         <div class="mycheckimg">
+         <img src="https://images-na.ssl-images-amazon.com/images/I/91gUsy8oIdL._SY606_.jpg"
+     </div>
+     </div>
+
+
+</div>
+
+</body>
+</html>
