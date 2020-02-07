d3255647cadd902fcfd49301cf6e50d02b8a05ed
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-07 15:39:11 -0500

    saving Manchester's code for feb 07

---
+++ projects/barcelona/jon.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Friday, Februrary 07, 2020</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<body>
+
+
+<div class="mybox">
+    <table class="table table-bordered">
+        <tr>
+            <th>ID</th>
+            <th>City</th>
+            <th>Country</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>Tashkent</td>
+            <td>Uzbekistan</td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>Brooklyn</td>
+            <td>USA</td>
+        </tr>
+
+        <tr>
+            <td>3</td>
+            <td>Moscow</td>
+            <td>Russia</td>
+        </tr>
+
+        <tr>
+            <td>4</td>
+            <td>Kiev</td>
+            <td>Ukraine</td>
+        </tr>
+
+        <tr>
+            <td>5</td>
+            <td>Paris</td>
+            <td>France</td>
+        </tr>
+
+    </table>
+</div>
+
+
+<style>
+
+    th{
+        background-color: #4f0000;
+        color: white;
+    }
+
+    .mybox{
+        width:80%;
+        margin: auto;
+        margin-top: 60px;
+    }
+
+    table{
+        background-color: #ffffff;
+        border-radius: 15px;
+    }
+
+    body{
+        background-color: #162c46;
+    }
+
+    tr:hover{
+        background-color: #c7f585;
+    }
+
+    tr:nth-child(even){
+        background-color: #eeeeee;
+    }
+</style>
+
+</body>
+</html>
