163aa24e3364ca2f9aff70ed897339366816b8a2
Fotima Maks <Maks@Fotimas-MBP.home>
2020-02-08 19:28:58 -0500

    saving

---
+++ projects/manchester/February/monday10.php
@@ -0,0 +1,13 @@
+<?php
+$get = $_GET??'N/A';
+$post = $_POST??'N/A';
+
+echo "<pre>";
+
+echo "<hr>";
+echo "Get dan kelgan danniy <br>";
+print_r($get);
+
+echo "<hr>";
+echo "Post dan kelgan danniy <br>";
+print_r($post);
