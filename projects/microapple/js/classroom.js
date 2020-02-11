aeb721f0252cb3d353a4f9d513d0f6ed0269da10
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-11 17:58:13 -0500

    saving microapple team code js

---
+++ projects/microapple/js/classroom.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    //JS Variables
+
+    //1) Integer (int) - only numbers between -100000 to 100000
+    //2) Strings  (string) - pretty much everything, alphanumeric values.
+    //3) Boolean (bool) - TRUE or FALSE. 1 or 0
+    //4) Float (double) - numbers with decimal points and not not a whole numbers. like Pi number - 3.14
+    //5) NULL - is also a variable.
+    //6) Arrays  (array) is a collection of or series of values.
+    //7) Object/Json
+
+
+    var age = 42;
+    var first_name = "Jon";
+    var last_name = 'Toshmatov';
+
+    alert(first_name +" "+ last_name + " is " + age +" years old");
+
+
+
+
+
+
+
+});
