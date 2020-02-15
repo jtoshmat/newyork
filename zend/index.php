7707fe92bfefae9c3ccd809f3624f70eb5609c2d
Umidochka Gaibullaeva <umidakhongaybullaeva@gmail.com>
2020-02-14 19:10:36 -0500

    saving

---
+++ zend/index.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL);
+echo "This square is {$square->width}00 centimeters broad.";
+echo "<hr>";
+class foo {
+    var $bar = 'I am bar.';
+}
+
+$foo = new foo();
+$bar = 'bar';
+$baz = array('foo', 'bar', 'baz', 'quux');
+echo "{$foo->$bar}\n";
+echo "{$foo->{$baz[1]}}\n";
+echo "<hr>";
+$str = 'This is a test.';
+$first = $str[8];
+echo $first;
+echo "<hr>";
+$str = 'This is still a test.';
+$last = $str[strlen($str)-9];
+echo $last;
+echo "<hr>";
+$str = 'abc';
+
+var_dump($str['1']);
+var_dump(isset($str['1']));
+
+echo "<hr>";
+$str = 'abc';
+var_dump($str['1.1']);
+var_dump(isset($str['1.1']));
+echo "<hr>";
+echo addcslashes("zoo['-']", 'z..A');
