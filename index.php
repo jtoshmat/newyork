99448c2620c25d9876a629b12f5a8786cd18193e
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-17 14:28:59 -0500

    saving login:

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-curl -H 'Authorization: JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAH4AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAzAAAABwAAAAEAAAAEAAAAEhY8sL5f05AOdyJ5faaQginAAAADm5FNSgfgU5CAO5xPVfC4GaV3vTeQ8nrh_6FVy5VKNA9lePdHSgrWs5RogBJ_y86HPQIfaA7m_J0TnCtAztSA8cw2YmGIdERX593GnO2yB-cXSAdlkHKBaeXqlBcgo4daTGiFcnmxapcEZh79tyCL9GXkMAh2LYIqUySONgCV1jkKNkjqdiYT0pV54wjnHqqGesnJLf34Sc-krOU-zZng5ipe-N6wmIADAAAAPtJ9yYkGu6x0yJ8CiQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU' \
-    -H 'Accept-Language: en_US' \
-    -H 'Content-Type: application/json' \
-    'https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=InvyZrKSz_TDrvUx8VjDC4mACCcC6YXd&redirect_uri=https://my-it-education.us/uber/callback'
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//Session
 
-
+?>
+</body>
+</html>
