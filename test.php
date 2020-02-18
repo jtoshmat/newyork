a8549527dbde839a578647acddc509bfcb4ccec4
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-02-18 01:21:51 -0500

    saving

---
+++ test.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="centered">
+    <div  id="editor" style="display: none; height: 300px">
+Your text goes here
+    </div>
+
+</div>
+
+
+<script src="ckeditor/ckeditor.js"></script>
+
+<script>
+    ClassicEditor
+        .create(document.querySelector('#editor'), {
+            // toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+        })
+        .then(editor => {
+            window.editor = editor;
+        })
+        .catch(err => {
+            console.error(err.stack);
+        });
+</script>
+
+</body>
+</html>
