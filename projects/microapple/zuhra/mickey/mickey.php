b28c379faced9e9f4c7800bca364aad464ca4881
zuhra <zuhramaksudi@gmail.com>
2020-02-22 19:25:44 -0500

    saving

---
+++ projects/microapple/zuhra/mickey/mickey.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../css/bootstrap.css">
+    <link rel="stylesheet" href="mickey.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="top">
+        <img src="https://www.greatgame.com/hubfs/Templates-2020/Home/GGOB%20logo%20(1).png" alt="">
+    </div>
+    <div class="menu">
+        <div class="home">Home</div>
+        <div class="home">Courses</div>
+        <div class="home">Videos</div>
+        <div class="home">Games</div>
+    </div>
+    <div class="background">
+        <div class="logo">
+            <span class="text">Welcome to <span class="text2"> MY-IT-<br> EDUCATION.US! </span></span>
+        </div>
+        <div class="image">
+            <img class="mickey"src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Mickey_Mouse.png/220px-Mickey_Mouse.png" alt="">
+        </div>
+        <div class="signature">
+            <img class="sign" src="https://upload.wikimedia.org/wikipedia/commons/0/00/Todd_Strasser_signature.png" alt="">
+        </div>
+    </div>
+</div>
+</body>
+</html>
