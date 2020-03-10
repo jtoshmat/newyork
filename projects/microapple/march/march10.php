a95548862d2c8c845489153b5353139e6396569a
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-10 18:41:12 -0400

    saving

--- projects/microapple/march/march10.php
+++ projects/microapple/march/march10.php
@@ -10,80 +10,102 @@
     <script src="/js/jquery.js"></script>
 </head>
 <body>
-
-<img id="car" src="https://i.pinimg.com/originals/70/63/d9/7063d9a730bbe5ddf084fa1f00f3199d.png">
-<hr>
-<p style="text-align: center">
-    <button class="btn btn-success" id="drive">Drive</button>
-
-    <button class="btn btn-danger" id="backup">Back Up</button>
-
-    <button class="btn btn-info" id="park">Park</button>
-</p>
-
-<div id="results">
-
+<div id="calculator">
+    <h2>My Calculator</h2>
+    <hr>
+    <form>
+        <p>
+            <input id="num1" required="required" type="number" name="num1" class="myinput">
+            <select id="operator" name="operator">
+                <option value="+">Add</option>
+                <option value="-">Minus</option>
+                <option value="*">Multiply</option>
+                <option value="/">Divide</option>
+            </select>
+            <input id="num2" required="required" type="number" name="num2" class="myinput">
+            <button id="go_button" class="btn btn-success" type="button">GO</button>
+        </p>
+    </form>
+    <div id="results"></div>
 </div>
-
 
 <script>
     $(function () {
-        $("#drive").click(function () {
-            drive('forward');
-        });
-        $("#park").click(function () {
-            alert("Park is working");
-        });
-        $("#backup").click(function () {
-            drive('reverse');
+        $("#go_button").click(function () {
+            var num1 = $("#num1").val();
+            var operator = $("#operator").val();
+            var num2 = $("#num2").val();
+
+            if (operator=='+'){
+                add(num1, operator, num2);
+            }else if (operator=='-'){
+                minus(num1, operator, num2);
+            }else if (operator=='/'){
+                divide(num1, operator, num2);
+            }else if (operator=='*'){
+                multiply(num1, operator, num2);
+            }
+            return false;
         });
     });
 
-    $("body").keydown(function (e) {
-        if (e.which == 37){
-            drive('forward');
-        }
 
-        if (e.which == 39){
-            drive('reverse');
-        }
-        e.preventDefault();
-    });
-
-    
-    function drive(dir) {
-        var pos = $("#car").position();
-        var f = pos.left - 500;
-        var r = pos.left + 500;
-
-
-        if (dir=='forward'){
-            $("#car").animate({left: f+'px'});
-        }
-
-        if (dir=='reverse'){
-            $("#car").animate({left: r+'px'});
-        }
-        return false;
-    }
-    
-    function park() {
-
-    }
-    
-    function backup() {
-
+    function add(num1, operator, num2) {
+        var c = eval(num1+ ' + ' + num2);
+        var resultText =  num1 + ' + ' + num2 + ' = ' + c;
+        displayResults(resultText);
     }
 
+    function minus(num1, operator, num2) {
+        var c = eval(num1+ ' - ' + num2);
+        var resultText =  num1 + ' - ' + num2 + ' = ' + c;
+        displayResults(resultText);
+    }
+
+    function multiply(num1, operator, num2){
+        var c = eval(num1+ ' * ' + num2);
+        var resultText =  num1 + ' * ' + num2 + ' = ' + c;
+        displayResults(resultText);
+    }
+
+    function divide(num1, operator, num2){
+        var c = eval(num1+ ' / ' + num2);
+        var resultText =  num1 + ' / ' + num2 + ' = ' + c;
+        displayResults(resultText);
+    }
+
+    function displayResults(result) {
+        $("#results").html(result);
+    }
 </script>
 
 <style>
-    #car{
-        position: absolute;
-        left: 0;
-        top: 110px;
+
+    #results {
+        background-color: #4e1510;
+        color: #ffcf24;
+        font-size: 200%;
+        font-weight: bolder;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    #calculator {
+        width: 600px;
+        height: auto;
+        margin: 50px auto;
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .myinput {
+        width: 70px;
+        height: 70px;
+        border: none;
+        border: 1px solid darkgrey;
+        border-radius: 5px;
+        padding: 5px
     }
 </style>
-
 </body>
 </html>
