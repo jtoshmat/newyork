58bf38f3b1bf9795f5bf4f6d665c59ec0e2b6c0e
sunnat <sunnatnyc08gmail.com>
2020-03-10 18:42:34 -0400

    saving

---
+++ projects/microapple/sunnat/calculator.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+</head>
+<body>
+<div id="calculator">Calculator</div>
+<hr>
+<select name="numbers">
+    <option selected value="">numbers</option>
+    <option value="01">01</option>
+    <option value="02">02</option>
+    <option value="03">03</option>
+    <option value="04">04</option>
+    <option value="05">05</option>
+    <option value="06">06</option>
+    <option value="07">07</option>
+    <option value="08">08</option>
+    <option value="09">09</option>
+    <option value="10">10</option>
+</select>
+<select name="elements">
+    <option selected value="elements"></option>
+    <option value="+"></option>
+    <option value="-"></option>
+    <option value="*"></option>
+    <option value=":"></option>
+</select>
+<select name="numbers1">
+    <option selected value="">numbers</option>
+    <option value="01">01</option>
+    <option value="02">02</option>
+    <option value="03">03</option>
+    <option value="04">04</option>
+    <option value="05">05</option>
+    <option value="06">06</option>
+    <option value="07">07</option>
+    <option value="08">08</option>
+    <option value="09">09</option>
+    <option value="10">10</option>
+</select>
+<div class="result"></div>
+<button class="btn btn-info">go</button>
+
+</body>
+<style>
+    .result{
+        width: 200px;
+        height: 100px;
+        border: 1px solid black;
+    }
+</style>
+</html>
