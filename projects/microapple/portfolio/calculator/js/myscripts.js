d0138739d205cd529125692d59958bb66ee319da
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-14 18:56:55 -0400

    saving

--- projects/microapple/portfolio/calculator/js/myscripts.js
+++ projects/microapple/portfolio/calculator/js/myscripts.js
@@ -1,6 +1,8 @@
 $(function () {
     $("#calculator td").click(function () {
         var input = $(this).data('num');
+        //Step 1: We collect the data-num='2' value from clicked td cell.
+        //Step 2: We call the function "dispatch" by passing the "input" which is the data-num='2' value
         dispatch(input);
     });
 });
@@ -8,7 +10,10 @@ var number = '';
 var operator = '';
 var is_new = true;
 function dispatch(input) {
+    //Step 3: We concatenate numbers and signs with "number" variable which starts with number = ''
     number+=input;
+
+    //Step 4: if "input" == plus or other signs then assign the signs to "operator" variable.
     if (input=='+'){
         operator = '+';
     }else if (input == '-'){
@@ -19,26 +24,17 @@ function dispatch(input) {
     }else if (input == '/'){
         operator = '/';
     }
+    //Step 5: If "input" == 'reset' then call the function "display" by passing the "input" variable and also reset parameter = true
     if (input == 'reset'){
         display(input, true);
         return false;
     }
+    //Step 6: If "input" == '=' then call the function "execute" and return false immediately.
     if (input == '='){
         execute();
         return false;
     }
-    if (input == '+'){
-        return false;
-    }
-    if (input == '-'){
-        return false;
-    }
-    if (input == '/'){
-        return false;
-    }
-    if (input == '*'){
-        return false;
-    }
+    //Step 7: Add the numbers 5 * 5 to $(".result")
     display(number);
 }
 function execute() {
