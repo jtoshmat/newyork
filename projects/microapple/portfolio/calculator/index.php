1e7cd5cd04212c73d6a342885ea7f07bf1bbbb63
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-14 17:23:51 -0400

    saving

--- projects/microapple/portfolio/calculator/index.php
+++ projects/microapple/portfolio/calculator/index.php
@@ -13,43 +13,41 @@
     <script src="js/myscripts.js"></script>
 </head>
 <body>
-
 <div id="calculator">
     <table class="table table-bordered">
         <tr>
             <td class="result" colspan="4">0</td>
         </tr>
         <tr>
-            <td class="signs">AC</td>
-            <td class="signs">+/-</td>
-            <td class="signs">&#37;</td>
-            <td class="orange_td">&#247;</td>
+            <td data-num="reset" class="signs">AC</td>
+            <td data-num="+/-" class="signs">&#43;/&#8722;</td>
+            <td data-num="%" class="signs">&#37;</td>
+            <td data-num="/" class="orange_td">&#247;</td>
         </tr>
         <tr>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td class="orange_td">&#215;</td>
+            <td data-num="7">7</td>
+            <td data-num="8">8</td>
+            <td data-num="9">9</td>
+            <td data-num="x" class="orange_td">&#215;</td>
         </tr>
         <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td class="orange_td">&#8722;</td>
+            <td data-num="4">4</td>
+            <td data-num="5">5</td>
+            <td data-num="6">6</td>
+            <td data-num="-" class="orange_td">&#8722;</td>
         </tr>
         <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td class="orange_td">&#43;</td>
+            <td data-num="1">1</td>
+            <td data-num="2">2</td>
+            <td data-num="3">3</td>
+            <td data-num="+" class="orange_td">&#43;</td>
         </tr>
         <tr>
-            <td colspan="2">0</td>
-            <td>.</td>
-            <td class="orange_td">&#61;</td>
+            <td data-num="0" colspan="2">0</td>
+            <td data-num=".">.</td>
+            <td data-num="=" class="orange_td">&#61;</td>
         </tr>
     </table>
 </div>
-
 </body>
 </html>
--- projects/microapple/portfolio/calculator/js/myscripts.js
+++ projects/microapple/portfolio/calculator/js/myscripts.js
@@ -1,3 +1,38 @@
 $(function () {
-    //alert("Jquery is working");
+    $("#calculator td").click(function () {
+        var input = $(this).data('num');
+        dispatch(input);
+    });
 });
+
+function dispatch(input) {
+
+
+    if (input == 'reset'){
+        display(input, true);
+        return false;
+    }
+    display(input);
+}
+
+
+
+function add(input) {
+}
+
+function minus(input) {
+}
+
+function multiply(input) {
+}
+
+function divide(input) {
+}
+
+function display(input, reset=false) {
+    if (reset){
+        $(".result").html('0');
+        return false;
+    }
+    $(".result").html(input);
+}
