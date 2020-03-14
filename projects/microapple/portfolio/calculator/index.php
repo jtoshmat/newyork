55c6d2829acde20744e86d8ec42c77965c414611
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-14 17:01:24 -0400

    saving my frontend calculator code

---
+++ projects/microapple/portfolio/calculator/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Basic Calculator</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="/js/jquery.js"></script>
+    <script src="js/myscripts.js"></script>
+</head>
+<body>
+
+<div id="calculator">
+    <table class="table table-bordered">
+        <tr>
+            <td class="result" colspan="4">0</td>
+        </tr>
+        <tr>
+            <td class="signs">AC</td>
+            <td class="signs">+/-</td>
+            <td class="signs">&#37;</td>
+            <td class="orange_td">&#247;</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td class="orange_td">&#215;</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td class="orange_td">&#8722;</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td class="orange_td">&#43;</td>
+        </tr>
+        <tr>
+            <td colspan="2">0</td>
+            <td>.</td>
+            <td class="orange_td">&#61;</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
---
+++ projects/microapple/portfolio/calculator/js/myscripts.js
@@ -0,0 +1,3 @@
+$(function () {
+    //alert("Jquery is working");
+});
