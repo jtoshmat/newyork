0a8cc95689d7fd14efde7764f58fa041a0f62260
Jon Toshmatov <jontoshmatov@yahoo.com>
2020-03-23 14:27:29 -0400

    saving Manchester calendar code

--- projects/manchester/oop/parents.php
+++ projects/manchester/oop/parents.php
@@ -3,17 +3,13 @@ namespace Shadiyarovs;
 
 class Parents{
 
-    private function earnMoney(){
-        return "Children must not know how much parents are making";
+    public function play(){
+        return 1001;
     }
 
-    public function eat(){
-        return "eating food together";
-    }
 
-    protected function celebrateBirthday(){
-        return 'Celebrating birthday is for our family only';
-    }
+
 }
+
 
 
---
+++ projects/manchester/portfolio/calendar/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Calendar</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/calendar.css">
+</head>
+<body>
+    <div id="calendar">
+      <div id="calendar_header">
+          <div class="left_div header_divs">
+              <i class="fas fa-circle circle1"></i>
+              <i class="fas fa-circle circle2"></i>
+              <i class="fas fa-circle circle3"></i>
+              <div class="header_btns">Calendars</div>
+              <div class="header_btns"> + </div>
+          </div>
+          <div class="middle_div header_divs"> buttons</div>
+          <div class="right_div header_divs"> search </div>
+      </div>
+      <div id="calendar_title">
+          <span class="title_month">March</span>
+          <span class="title_year">2020</span>
+      </div>
+      <div id="calendar_table">
+          <table class="table">
+              <?php
+                $days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+              ?>
+
+              <tr>
+                  <?foreach ($days as $day):?>
+                    <th><?=$day?></th>
+                  <?endforeach;?>
+              </tr>
+
+              <?php $days = 0; for($i=0; $i<5; $i++):?>
+                  <tr>
+                  <?php for($y=0; $y<7; $y++): $days++;?>
+                      <? if (date('d') == $days):?>
+                            <td class="calendar_today">
+                                <div><?=$days?></div>
+                            </td>
+                      <?else:?>
+                          <td><div><?=$days?></div></td>
+                      <?endif?>
+                  <?endfor;?>
+                  </tr>
+              <?endfor;?>
+
+
+          </table>
+      </div>
+      <div id="calendar_footer">
+          &copy; Shadiyarov.us <?=date('Y');?>
+      </div>
+    </div>
+</body>
+</html>
