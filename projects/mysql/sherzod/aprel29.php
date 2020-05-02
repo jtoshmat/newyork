57f63bed9d30efd35186d18e64681d97f68f7360
Sherzod Burkutboyev <SherzodBurkutboyev@Sherzods-MBP.home>
2020-05-02 17:38:30 -0400

    saving

---
+++ projects/mysql/sherzod/aprel29.php
@@ -0,0 +1,24 @@
+<?php
+/*
+$ism = " Jon";
+$job = "programmer";
+$hobby = "walking in the park and playing tennis";
+$dream = "travel the world";
+
+echo "Hello, my name is $ism and I am $job. My hobby is $hobby. I will $dream .";
+*/
+
+
+
+$name = "Doston";
+$cr_year = 2020;
+$dob = 1997;
+$job = "student";
+$hobby = "playing tennis";
+
+print "My cousin is $name. He is $job. In his free times he likes $hobby . His age is "; echo $cr_year-$dob; print".";
+print '<br>';
+print "Five squared is "; echo 5*5;
+print '<br>';
+print 'Half of 10 is '; echo 10/2;
+?>
