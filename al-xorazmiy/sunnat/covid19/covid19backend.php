a4a384eb880cd623a86dd01621c6dd925f91ae0e
sunnat <sunnatnyc08gmail.com>
2020-06-01 18:48:12 -0400

    saving

--- al-xorazmiy/sunnat/covid19/covid19backend.php
+++ al-xorazmiy/sunnat/covid19/covid19backend.php
@@ -1,19 +1,17 @@
 <?php
-$sign = $_GET["sign"]??"virgo";
-
+$country = $_GET['country']??"Uzbekistan";
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=$sign&day=today",
+    CURLOPT_URL => "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-04-01&name=Italy",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => "",
+    CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => array(
         "x-rapidapi-host: covid-19-data.p.rapidapi.com",
         "x-rapidapi-key: a8ffce9bebmshf7ec9d45dc197fcp1ce2f9jsnd6da02ec8b57"
@@ -25,8 +23,14 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
+
+
 if ($err) {
     echo "cURL Error #:" . $err;
 } else {
-    echo $response;
+    $response = json_decode($response);
 }
+
+/*echo "<pre>";
+print_r($response);
+exit;
